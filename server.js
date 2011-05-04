30c84c657f3ea5348b12d39853138583459ee830
Alex Galonsky <alex@galonsky.com>
2011-05-04 12:34:32 -0400

    Simple add form works, and gives URL

--- server.js
+++ server.js
@@ -17,4 +17,19 @@ app.get('/add', function(req, http_res) {
     http_res.render('add');
 });
 
+app.post('/add', function(req, http_res) {
+    var fields = {body: req.body.doc.body};
+    api.add_document(fields, req.body.doc.title, function(err, res, url) {
+        if(err) {
+            http_res.render('error', {
+                locals: {message: err}
+            });
+        } else {
+            http_res.render('add', {
+                locals: {url: url}
+            });
+        }
+    })
+})
+
 app.listen(4000);

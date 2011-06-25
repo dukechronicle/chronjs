9192bbc92703a87c0325c96d752e4f800e8a61ab
Dean Chen <dean@ocirs.com>
2011-06-25 13:09:40 -0400

    restructured custom modules layout

--- server.js
+++ server.js
@@ -5,10 +5,10 @@ var PORT = process.env.PORT || 4000;
 var express = require('express');
 require('express-namespace');
 var stylus = require('stylus');
-var nimble = require('nimble');
 
 /* require internal nodejs modules */
-var api = require('./api/api.js');
+var api = require('./thechronicle_modules/api');
+var admin = require('./thechronicle_modules/admin')
 
 /* express configuration */
 var app = express.createServer();
@@ -104,6 +104,20 @@ app.get('/', function(req, http_res) {
     });
 });
 
+app.get('/article/:url', function(req, http_res) {
+    var url = req.params.url;
+    
+    api.doc_for_url(url, function(err, doc) {
+        if(err) {
+            _error(http_res, err);
+        } else {
+            http_res.render('article', {
+                locals: {doc: doc}
+            });
+        }
+    });
+});
+
 app.get('/article/:url/edit', function(req, http_res) {
     var url = req.params.url;
     api.doc_for_url(url, function(err, doc) {
@@ -123,101 +137,10 @@ app.get('/article/:url/edit', function(req, http_res) {
         }
     });
 });
-
-app.get('/article/:url', function(req, http_res) {
-    var url = req.params.url;
-    
-    api.doc_for_url(url, function(err, doc) {
-        if(err) {
-            _error(http_res, err);
-        } else {
-            http_res.render('article', {
-                locals: {doc: doc}
-            });
-        }
-    });
-});
 /*** !FRONTEND ***/
 
 /*** ADMIN ***/
-app.namespace('/admin', function() {
-	app.get('/addbin', function(req, http_res) {
-	    api.bin.create(req.query.addbin, function(err, res) {
-	        if(err) {
-	            _error(http_res, err);
-	        } else {
-	            http_res.redirect('/add');
-	        }
-	    })
-	});
-	
-	app.get('/add', function(req, http_res) {
-	    api.bin.list(function(err, bins) {
-	        http_res.render('admin/add', {
-	            locals: {bins: bins}
-	        });
-	    });
-	});
-	
-	app.get('/manage', function(req, http_res) {
-	    api.all_docs_by_date(function(err, res) {
-	        if(err) {
-	            _error(http_res, err);
-	        } else {
-	            http_res.render('admin/manage', {
-	                locals: {docs: res}
-	            });
-	        }
-	    });
-	});
-	
-	app.post('/edit', function(req, http_res) {
-	    var id = req.body.doc.id;
-	    var new_bins = req.body.doc.bins;
-	    if(!(new_bins instanceof Array)) { //we will get a string if only one box is checked
-	        new_bins = [new_bins];
-	    }
-	    var fields = {
-	        title: req.body.doc.title,
-	        body: req.body.doc.body,
-	        bins: new_bins
-	    };
-	    api.edit_document(id, fields, function(err, res) {
-	        if(err) {
-	            _error(http_res, err);
-	        } else {
-	            http_res.redirect('/article/' + res.merge[1] + '/edit');
-	        }
-	    });
-	});
-	
-	app.post('/add', function(req, http_res) {
-	    var fields = {body: req.body.doc.body};
-	    api.add_document(fields, req.body.doc.title, function(err, res, url) {
-	        if(err) {
-	            _error(http_res, err);
-	        } else {
-	            var bins = req.body.doc.bins;
-	            if(bins) {
-	                var fcns = [];
-	                if(!(bins instanceof Array)) { //we will get a string if only one box is checked
-	                    bins = [bins];
-	                }
-	                
-	                api.bin.add(res.id, bins, function(add_err, add_res) {
-	                    if(add_err) {
-	                        _error(http_res, add_err);
-	                    } else {
-	                        http_res.redirect('article/' + url);
-	                    }
-	                });
-	            } else {
-	                http_res.redirect('article/' + url);
-	            }
-	        }
-	    });
-	});
-});
+app = admin.init(app);
 
 /*** !ADMIN ***/
 

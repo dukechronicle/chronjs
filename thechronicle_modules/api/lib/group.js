106afd11cae45262d24ffd83b585dc9b3eb89290
Dean Chen <dean@ocirs.com>
2011-06-26 14:39:19 -0400

    moved more db level group functions to db-abstract/lib/group.js

--- thechronicle_modules/api/lib/group.js
+++ thechronicle_modules/api/lib/group.js
@@ -48,70 +48,12 @@ group.create = function(group, callback) {
     });
 }
 
-function _addRemoveLogic(docid, group, callback, dbres, logic) {
 
-    var children = dbres[0].value.children;
-    //add to group document
-    logic(children, docid);
-    nimble.series([
-    function(acallback) {
-        db.merge(dbres[0].id, {
-            children: children
-        },
-        acallback);
-    },
-    function(acallback) {
-        db.get(docid,
-        function(err2, res2) {
-            if (err2) {
-                acallback(err2, null);
-            }
-            else {
-                var groups = res2.groups;
-                if(!groups) {
-                    groups = [];
-                }
-                logic(groups, group);
-                db.merge(docid, {
-                    groups: groups
-                },
-                acallback);
-            }
-        });
-    }], 
-    callback);
-}
-
-function _addToGroup(docid, group, callback) {
-    //check if group exists
-    db.group.list({
-        key: group
-    },
-    function(err, res) {
-        if(err) {
-            callback(err, null);
-        }
-        else if (res.length == 0) {
-            callback("group does not exist", null);
-        }
-        else {
-            var children = res[0].value.children;
-            if (children.indexOf(docid) != -1) {
-                callback("Document already in this group", null);
-            }
-            else {
-                _addRemoveLogic(docid, group, callback, res, function(arr, obj) {
-                    arr.push(obj);
-                });
-            }
-        }
-    });
-}
 
 group.add = function(docid, groups, callback) {
     nimble.map(groups, function(item, cbck) {
         cbck(null, function(acallback) {
-            _addToGroup(docid, item, acallback);
+            db.group.add(docid, item, acallback);
         });
     }, function(map_err, results) {
         nimble.series(results, function(ser_err, ser_res) {
@@ -120,35 +62,10 @@ group.add = function(docid, groups, callback) {
     });
 }
 
-function _removeFromGroup(docid, group, callback) {
-    //check if group exists
-    db.group.list({
-        startkey: group,
-        endkey: group
-    },
-    function(err, res) {
-        if (res.length == 0) {
-            callback("group does not exist", null);
-        }
-        else {
-            var children = res[0].value.children;
-            if (children.indexOf(docid) == -1) {
-                callback("Document not in this group", null);
-            }
-            else {
-                _addRemoveLogic(docid, group, callback, res, function(arr, obj) {
-                    var index = arr.indexOf(obj);
-                    arr.splice(index, 1);
-                });
-            }
-        }
-    });
-}
-
 group.remove = function(docid, groups, callback) {
     nimble.map(groups, function(item, cbck) {
         cbck(null, function(acallback) {
-            _removeFromGroup(docid, item, acallback);
+            db.group.remove(docid, item, acallback);
         });
     }, function(map_err, map_res) {
         nimble.series(map_res, callback);
--- thechronicle_modules/db-abstract/lib/group.js
+++ thechronicle_modules/db-abstract/lib/group.js
@@ -5,8 +5,96 @@ var group = exports;
 // lists all groups with given query options
 group.list = function(options, callback) {
     db.view('articles/list_groups', options,
+	    function(err, res) {
+	        callback(err, res);
+	    }
+	);
+}
+
+// add document to group
+group.add = function (docId, group, callback) {
+    //check if group exists
+    group.list({
+        key: group
+    },
     function(err, res) {
-        callback(err, res);
-    }
-);
+        if(err) {
+            callback(err, null);
+        }
+        else if (res.length == 0) {
+            callback("group does not exist", null);
+        }
+        else {
+        	// if group exists and doc isn't already in group 
+        	// add docid to children array of group
+            var children = res[0].value.children;
+            if (children.indexOf(docId) != -1) {
+                callback("Document already in this group", null);
+            }
+            else {
+                _addRemoveLogic(docId, group, callback, res, function(arr, obj) {
+                    arr.push(obj);
+                });
+            }
+        }
+    });
+}
+
+group.remove = function(docId, group, callback) {
+    //check if group exists
+    db.group.list({
+        startkey: group,
+        endkey: group
+    },
+    function(err, res) {
+        if (res.length == 0) {
+            callback("group does not exist", null);
+        }
+        else {
+            var children = res[0].value.children;
+            if (children.indexOf(docId) == -1) {
+                callback("Document not in this group", null);
+            }
+            else {
+                _addRemoveLogic(docId, group, callback, res, function(arr, obj) {
+                    var index = arr.indexOf(obj);
+                    arr.splice(index, 1);
+                });
+            }
+        }
+    });
+}
+
+// TODO refactor this to make sense
+function _addRemoveLogic(docid, group, callback, dbres, logic) {
+	var children = dbres[0].value.children;
+    //add to group document
+    logic(children, docid);
+    nimble.series([
+    function(acallback) {
+        db.merge(dbres[0].id, {
+            children: children
+        },
+        acallback);
+    },
+    function(acallback) {
+        db.get(docid,
+        function(err2, res2) {
+            if (err2) {
+                acallback(err2, null);
+            }
+            else {
+                var groups = res2.groups;
+                if(!groups) {
+                    groups = [];
+                }
+                logic(groups, group);
+                db.merge(docid, {
+                    groups: groups
+                },
+                acallback);
+            }
+        });
+    }], 
+    callback);
 }

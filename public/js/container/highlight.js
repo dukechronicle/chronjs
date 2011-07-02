3134ce1b82429eeabc74a9164b1db80ff44b4bcb
Dean Chen <dean@ocirs.com>
2011-07-02 18:09:13 -0400

    use css3 for border fade instead, cross browser compatible

--- public/js/container/highlight.js
+++
@@ -1,4 +0,0 @@
-$(document).ready(function() {
-	$('body a.list-story').mouseover(function() {$(this).animate({'border-right-color': '#4D90F0'}, 200)});
-	$('body a.list-story').mouseout(function() {$(this).animate({'border-right-color': 'white'}, 200)});
-});

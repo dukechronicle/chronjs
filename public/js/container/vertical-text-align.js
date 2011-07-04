1b66105f612ce3d3e6e8eaa2ac644954d56033a1
Dean Chen <dean.chen@duke.edu>
2011-07-04 12:09:54 -0400

    styling updates for hover on twitter box and navigation, lighted link hover color

--- public/js/container/vertical-text-align.js
+++ public/js/container/vertical-text-align.js
@@ -1,11 +1,22 @@
-$(document).ready(
-function position() {
+$(function() {
+	// position vertical labels
 	$(".vertical-container").each(function(i) {
 		var height = $(this).css('width');
 		var topSpacing = 3;
 		height = parseInt(height.substring(0, height.length - 2), 10) + topSpacing;
-		console.log(height);
 		$(this).css('top', height + "px");
 	});
-}
-);
+/*
+	$(".hover-highlight").each(function(i) {
+		var element = $(this);
+		element.onmouseover(function() {
+			element.find('a').addClass('selected');
+			element.find('p').addClass('selected');
+		});
+
+		element.onmouseout(function() {
+			element.find('a').removeClass('selected');
+			element.find('p').removeClass('selected');
+		})
+	});*/
+});

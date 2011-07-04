1ae3942745b6099601ad927e3fa1b3a1f5ed3ea2
Dean Chen <dean.chen@duke.edu>
2011-07-03 21:45:44 -0400

    functional slideshow

---
+++ public/js/slideshow/slideshow.js
@@ -0,0 +1,33 @@
+$(function(){
+	var currentSlide = 0;
+	var nextSlide = 0;
+	var totalSlides = $('.slideshow .slides img').size();
+
+	// start by showing only the first sldie
+    $('.slideshow .slides img:gt(0)').hide();
+
+	/*
+    setInterval(function(){
+	    nextSlide = currentSlide + 1;
+	    if (nextSlide === totalSlides) nextSlide = 0;
+			showSlide(nextSlide);
+		}, 6000);*/
+
+	// generic function used to switch from one slide to anoter
+	function showSlide(newSlide) {
+		$('.slideshow .slides img:eq(' + currentSlide + ')').fadeOut('slow');
+	    $('.slideshow .slides img:eq(' + newSlide + ')').fadeIn('slow');
+
+	    $('.slideshow .headlines a:eq(' + currentSlide + ')').removeClass('active');
+	    $('.slideshow .headlines a:eq(' + newSlide + ')').addClass('active');
+
+		currentSlide = newSlide;
+	}
+
+	// bind headlines to slideshow switches
+	$('.slideshow .headlines a').each(function(index) {
+		$(this).click(function() {
+			showSlide(index);
+		})
+	});
+});

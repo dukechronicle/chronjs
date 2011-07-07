929afd4044e29b488bf2ff7870510f3394d900b8
Joe Levy <jodoglevy@gmail.com>
2011-07-07 01:07:14 -0700

    basic client side part of drag and drop + upload

--- public/js/html5upload.js
+++ public/js/html5upload.js
@@ -4,71 +4,60 @@ var IMAGE_HTML = "<img id='tempPreview' />";
 var dropLabelStartText = "";
 
 $(document).ready(function() {
-	var dropbox = document.getElementById("dropbox")
 	dropLabelStartText = $("#droplabel").text();
 
 	// init event handlers
-	dropbox.addEventListener("dragenter", dragEnter, false);
-	dropbox.addEventListener("dragexit", dragExit, false);
-	dropbox.addEventListener("dragover", dragOver, false);
-	dropbox.addEventListener("drop", drop, false);
+	var dropbox = document.getElementById("dropbox");
+
+	dropbox.addEventListener("dragenter", function(evt) {
+		evt.stopPropagation();
+		evt.preventDefault();
+	}, false);
+
+	dropbox.addEventListener("dragexit", function(evt) {
+		evt.stopPropagation();
+		evt.preventDefault();
+	}, false);
+
+	dropbox.addEventListener("dragover", function(evt) {
+		evt.stopPropagation();
+		evt.preventDefault();
+	}, false);
+
+	dropbox.addEventListener("drop", function(evt) {
+		evt.stopPropagation();
+		evt.preventDefault();
+
+		var files = evt.dataTransfer.files;
+		var count = files.length;
+
+		// Only call the handler if 1 or more files was dropped.
+		if (count > 0) {
+			imagesLeft = count;
+			
+			if(count == 1) {
+				$("#droplabel").text("Processing " + files[0].name);
+			}
+			else {
+				$("#droplabel").text("Processing files...");
+			}
+
+			$.each(files,function(index,file) {
+				// begin the read operation
+				var reader = new FileReader();
+
+				// init the reader event handlers
+				reader.onprogress = handleReaderProgress;
+				reader.onloadend = handleReaderLoadEnd;
+
+				reader.readAsDataURL(file);
+			});
+		}
+	}, false);
 
 	// init the widgets
 	//$("#progressbar").progressbar();
 });
-
-function dragEnter(evt) {
-	evt.stopPropagation();
-	evt.preventDefault();
-}
-
-function dragExit(evt) {
-	evt.stopPropagation();
-	evt.preventDefault();
-}
-
-function dragOver(evt) {
-	evt.stopPropagation();
-	evt.preventDefault();
-}
-
-function drop(evt) {
-	evt.stopPropagation();
-	evt.preventDefault();
-
-	var files = evt.dataTransfer.files;
-	var count = files.length;
-
-	// Only call the handler if 1 or more files was dropped.
-	if (count > 0)
-		handleFiles(files);
-}
-
-
-function handleFiles(files) {
-	imagesLeft = files.length;
-
-	for(var i = 0; i < files.length; i ++)	
-	{
-		if(files.length == 1)
-		{
-			$("#droplabel").text("Processing " + files[0].name);
-		}
-		else
-		{
-			$("#droplabel").text("Processing files...");
-		}
-
-		var reader = new FileReader();
-
-		// init the reader event handlers
-		reader.onprogress = handleReaderProgress;
-		reader.onloadend = handleReaderLoadEnd;
-
-		// begin the read operation
-		reader.readAsDataURL(files[i]);
-	}
-}
 
 function handleReaderProgress(evt) {
 	if (evt.lengthComputable) {
@@ -81,20 +70,36 @@ function handleReaderProgress(evt) {
 function handleReaderLoadEnd(evt) {
 	//$("#progressbar").progressbar({ value: 100 });
 
-	$("#pictureholder").append(IMAGE_HTML);
+	$.ajax({
+   		type: "POST",
+  		url: "/test-upload",
+   		data: "imagedata="+evt.target.result,
 
-	var img = $("#tempPreview");
-	img.attr("id","picture"+numImages);
-	numImages++;
+		error: function(msg) {
+			alert(msg);
+		},
 
-	img.attr("src",evt.target.result);
-	img.attr("width","200");
-	img.attr("height","200");
-	
-	imagesLeft --;
+   		success: function(msg) {
+     			$("#pictureholder").append(IMAGE_HTML);
 
-	if(imagesLeft == 0)
-	{
-		$("#droplabel").text(dropLabelStartText);
-	}
+			var img = $("#tempPreview");
+
+			img.hide();
+			img.attr("id","picture"+numImages);
+			numImages++;
+
+			img.attr("src",evt.target.result);
+			img.attr("width","200");
+			img.attr("height","200");
+
+			img.fadeIn('slow');
+
+			imagesLeft --;
+
+			if(imagesLeft == 0)	
+			{
+				$("#droplabel").text(dropLabelStartText);	
+			}
+   		}
+ 	});	
 }

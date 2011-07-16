18f826cecf820da9a40c1f121fa14f5b4d3d02ef
Dean Chen <dean.chen@duke.edu>
2011-07-16 14:05:22 -0400

    do not require redis connection

--- thechronicle_modules/config/lib/config.js
+++ thechronicle_modules/config/lib/config.js
@@ -5,45 +5,64 @@ var configHashKey = "config:dev";
 
 // parse enviroment variable to extract login information
 var redisUrl = process.env.REDISTOGO_URL || process.env.REDIS_URL;
-if(!redisUrl) throw "No Redis URL specified...";
-redisUrl = url.parse(redisUrl);
-redisUrl.auth = redisUrl.auth.split(":");
 
-// create redis client and authenticate
-var client = redis.createClient(redisUrl.port, redisUrl.hostname);
-client.auth(redisUrl.auth[1], function(err, reply) {
-	if (err) {
-		throw "Error Connection to redis: " + err;
-	}
-});
+if (redisUrl) {
+	redisUrl = url.parse(redisUrl);
+	redisUrl.auth = redisUrl.auth.split(":");
 
-var config = exports;
-var configCache = {};
-
-config.init = function(callback) {
-	client.HGETALL(configHashKey, function(err, obj) {
-		if (err) throw err;
-		configCache = obj;
-		if (typeof callback !== "undefined") {
-			callback(err, obj);
+	// create redis client and authenticate
+	var client = redis.createClient(redisUrl.port, redisUrl.hostname);
+	client.auth(redisUrl.auth[1], function(err, reply) {
+		if (err) {
+			throw "Error connecting to redis: " + err;
 		}
 	});
+	client.on("error", function (err) {
+        console.log("Error " + err);
+	});
+
+} else {
+	client = null;
+}
+
+var config = exports;
+var configCache = null;
+
+config.init = function(callback) {
+	if (client) {
+		client.HGETALL(configHashKey, function(err, obj) {
+			if (err) throw err;
+
+			if (configCache === null) {
+				console.log("Loaded configuration:");
+				console.log( JSON.stringify(obj, null, "\t"));
+			}
+			configCache = obj;
+
+			if (typeof callback !== "undefined") {
+				callback(err, obj);
+			}
+		});
+	} else {
+		if (configCache === null) configCache = {};
+		callback();
+	}
 };
 
-client.on("error", function (err) {
-    console.log("Error " + err);
-});
 
-client.HGETALL(configHashKey, function(err, obj) {
-	if (err) throw err;
-	console.log(obj);
-});
 
 config.set = function(variable, value, callback) {
-	client.HSET(configHashKey, variable, value, function(err, result) {
-		if (err) throw err;
-		config.init(callback);
-	});
+	if (client) {
+		client.HSET(configHashKey, variable, value, function(err, result) {
+			if (err) throw err;
+			config.init(callback);
+		});
+	} else {
+		configCache[variable] = value;
+		if (typeof callback !== "undefined") {
+			callback(null);
+		}
+	}
 };
 
 config.get = function(variable, defaultValue) {
@@ -51,7 +70,6 @@ config.get = function(variable, defaultValue) {
 	if (process.env[variable]) {
 		return process.env[variable];
 	} else if (configCache[variable]) {
-		console.log(configCache);
 		return configCache[variable];
 	} else {
 		return defaultValue;

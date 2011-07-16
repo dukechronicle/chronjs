cfab2318295132a8c998b512148d74cea56b18ce
Dean Chen <dean.chen@duke.edu>
2011-07-16 17:01:03 -0400

    improved couchdb connection logic by using url.parse instead of regular expression

--- thechronicle_modules/db-abstract/lib/db-abstract.js
+++ thechronicle_modules/db-abstract/lib/db-abstract.js
@@ -2,30 +2,30 @@ var cradle = require('cradle');
 var _ = require('underscore');
 var db_design = require('./db_design');
 var config = require('../../config');
+var url = require("url");
 
 var DATABASE = config.get("COUCHDB_DATABASE", "chronicle");
 
 // parse environment variable CLOUDANT_URL OR COUCHDB_URL to extract authentication information
 function connect(database) {
-	var cloudantUrlRegEx = new RegExp('(.*?)://(.*?):(.*?)@(.*)')
-	var connect_url = process.env.CLOUDANT_URL || config.get("COUCHDB_URL");
-	var cloudant_auth = cloudantUrlRegEx.exec(connect_url);
+	var couchdbUrl = process.env.CLOUDANT_URL || config.get("COUCHDB_URL");
+	if(!couchdbUrl) throw "No Cloudant URL specified...";
+
+	couchdbUrl = url.parse(couchdbUrl);
+	couchdbUrl.auth = couchdbUrl.auth.split(":");
+
+	(couchdbUrl.protocol === "https") ? couchdbUrl.port = 443 : couchdbUrl.port = 80;
 	
-	if(!cloudant_auth) throw "No Cloudant URL specified...";
-	var port;
-	(cloudant_auth[1] === 'https') ? port = 443 : port = 80;
-	
-	var conn = new(cradle.Connection)(cloudant_auth[1] + '://' + cloudant_auth[4], port, {
-		auth: {username: cloudant_auth[2], password: cloudant_auth[3]}
+	var conn = new (cradle.Connection)(couchdbUrl.protocol + '//' + couchdbUrl.hostname, couchdbUrl.port, {
+		auth: {username: couchdbUrl.auth[0], password: couchdbUrl.auth[1]}
 	}); 
 	
 	var db = conn.database(database);
-	
+
+	// initialize database if it doesn't already exist
 	db.exists(function (error,exists)
 	{
-	  	if(error) {
-			console.log(error);
-		}
+	  	if(error) console.log(error);
 
 		if(!exists) {
 			db.create();
@@ -35,7 +35,7 @@ function connect(database) {
    	return db;	
 }
 
-// all functions defined inside of db variable will be available as a module function
+// connect returns the cradle object
 _.extend(exports, connect(DATABASE));
 
 var db = exports;

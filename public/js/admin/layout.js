82a1c9d6cf2b11077f8decad2ce392f178cbb203
Dean Chen <deanchen@Deans-MacBook-Air.local>
2011-08-01 00:39:37 -0400

    drag and drop to different containers work

--- public/js/admin/layout.js
+++ public/js/admin/layout.js
@@ -32,8 +32,6 @@ $(function() {
         if (e.stopPropagation) e.stopPropagation();
 
         var docId = e.dataTransfer.getData("Text");
-        var groupName = $(this).data("groupname");
-        var nameSpace = ["Layouts", "Frontpage"];
 
         var element = $("#" + docId);
 
@@ -43,26 +41,78 @@ $(function() {
         element.addClass("exists");
         element.attr("draggable", false);
         //$(this).append(element.get(0));
-        
-        $.post("/admin/group/add", {docId: docId, groupName: groupName, nameSpace: nameSpace, weight: 0});
+
+
+
+        $.post("/admin/group/add", {
+            docId: docId,
+            groupName: $(this).data("groupname"),
+            nameSpace: ["Layouts", "Frontpage"],
+            weight: containerElement.index()
+        });
         
         $(this).removeClass("over");
     });
 
     $("#layout").delegate(".story", "drop", function(e) {
         if (e.stopPropagation) e.stopPropagation();
-
+        var _this = this;
         var docId = e.dataTransfer.getData("Text");
-        var groupName = $(this).data("groupname");
-        var nameSpace = ["Layouts", "Frontpage"];
-
         var element = $("#" + docId);
+        var newElement = element.clone();
+        var nextSibling;
+        
+        newElement.insertBefore($(this));
 
-        element.addClass("exists");
-        element.insertBefore($(this));
-       // $(this).append(element.get(0));
-        console.log(groupName);
-       // $.post("/admin/group/add", {docId: docId, groupName: groupName, nameSpace: nameSpace, weight: 0});
+        // story has changed groups
+        if (element.parent().data("groupname") !== $(this).parent().data("groupname")) {
+            var oldElementParent = element.parent();
+            nextSibling = element.next();
+
+            $.post("/admin/group/remove", {
+                docId: element.attr("id"),
+                groupName: oldElementParent.data("groupname"),
+                nameSpace: ["Layouts", "Frontpage"]
+            }, function() {
+                $.post("/admin/group/add", {
+                docId: docId,
+                groupName: $(_this).parent().data("groupname"),
+                nameSpace: ["Layouts", "Frontpage"],
+                weight: newElement.index()
+            });
+            });
+            element.remove();
+
+            if (nextSibling.length>0) {
+                do {
+                    $.post("/admin/group/add", {
+                        docId: nextSibling.attr("id"),
+                        groupName: oldElementParent.data("groupname"),
+                        nameSpace: ["Layouts", "Frontpage"],
+                        weight: nextSibling.index()
+                    });
+                } while ((nextSibling = nextSibling.next()) && (nextSibling.length > 0));
+            }
+        } else {
+
+            $.post("/admin/group/add", {
+                docId: docId,
+                groupName: $(this).parent().data("groupname"),
+                nameSpace: ["Layouts", "Frontpage"],
+                weight: newElement.index()
+            });
+        }
+
+        nextSibling = newElement;
+        while ((nextSibling = nextSibling.next()) && (nextSibling.length > 0)) {
+
+            $.post("/admin/group/add", {
+                docId: nextSibling.attr("id"),
+                groupName: $(this).parent().data("groupname"),
+                nameSpace: ["Layouts", "Frontpage"],
+                weight: nextSibling.index()
+            });
+        }
 
         $(this).removeClass("over");
     });

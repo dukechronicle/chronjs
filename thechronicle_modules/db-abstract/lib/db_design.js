4460ef54345e0021a5814c93f9e68de8f87c92d4
Dean Chen <dean@ocirs.com>
2011-08-07 10:59:05 -0400

    Can't store regular variable in design document.

--- thechronicle_modules/db-abstract/lib/db_design.js
+++ thechronicle_modules/db-abstract/lib/db_design.js
@@ -1,81 +1,80 @@
 var DESIGN_DOCUMENT_NAME = '_design/articles';
 
 var views = {
-    version: 1.3, // update this version whenever you change to views object.
-    taxonomy: {
+	taxonomy: {
 	map: function(doc) {
-	    if (doc.taxonomy) {
-		    for (var i in doc.taxonomy) {
-	            emit([doc.taxonomy[i], doc.created], doc);
-		    }
-	    }
+	if (doc.taxonomy) {
+		for (var i in doc.taxonomy) {
+			emit([doc.taxonomy[i], doc.created], doc);
+		}
 	}
-    },
-    // return all doc ids keyed by doc url if one exists
-    urls: {
-	map: function(doc) {
-	    if(doc.urls) {
-	        for(var i in doc.urls) {
-	            emit(doc.urls[i], doc._id);
-	        }
-	    }
-	}
-    },
-    // return all doc ids keyed by document author name if one exists
-    authors: {
-	map: function(doc) {
-	    if(doc.authors) {
-	        for(var i in doc.authors)
-	            emit(doc.authors[i], doc._id);
-	    }
-	}
-    },
-    // get the uuid of all children keyed by fully qualified group name
-    groups: {
-	map: function(doc) {
-           if(doc.groups) {
-               doc.groups.forEach(function(group) {
-                   emit(group, {title: doc.title});
-               });
-           }
+}
 	},
-       reduce: function(keys, values, rereduce) {
-         if (rereduce) {
-           return sum(values);
-         } else {
-           return values.length;
-         }
-       }
-    },
-    // return articles keyed by date
-    all_by_date: {
+	// return all doc ids keyed by doc url if one exists
+	urls: {
 	map: function(doc) {
-	    if(doc.urls) {
-	        emit(doc.created, doc);
-	    }
+		if(doc.urls) {
+			for(var i in doc.urls) {
+				emit(doc.urls[i], doc._id);
+			}
+		}
 	}
-    },
-    image_originals: {
+	},
+	// return all doc ids keyed by document author name if one exists
+	authors: {
 	map: function(doc) {
-	    if(doc.imageVersions) {
-	        emit(doc.name, doc);
-	    }
+		if(doc.authors) {
+			for(var i in doc.authors)
+				emit(doc.authors[i], doc._id);
+		}
 	}
-    },
-    photographers: {
+	},
+	// get the uuid of all children keyed by fully qualified group name
+	groups: {
 	map: function(doc) {
-	    if(doc.photographer) {
-	        emit(doc.photographer, doc);
-	    }
+		   if(doc.groups) {
+			   doc.groups.forEach(function(group) {
+				   emit(group, {title: doc.title});
+			   });
+		   }
+	},
+	   reduce: function(keys, values, rereduce) {
+		 if (rereduce) {
+		   return sum(values);
+		 } else {
+		   return values.length;
+		 }
+	   }
+	},
+	// return articles keyed by date
+	all_by_date: {
+	map: function(doc) {
+		if(doc.urls) {
+			emit(doc.created, doc);
+		}
 	}
-    },
-    image_versions: {
-        map: function(doc) {
-            if(doc.original) {
-                emit(doc.original, doc);
-            }
-        }
-    }
+	},
+	image_originals: {
+	map: function(doc) {
+		if(doc.imageVersions) {
+			emit(doc.name, doc);
+		}
+	}
+	},
+	photographers: {
+	map: function(doc) {
+		if(doc.photographer) {
+			emit(doc.photographer, doc);
+		}
+	}
+	},
+	image_versions: {
+		map: function(doc) {
+			if(doc.original) {
+				emit(doc.original, doc);
+			}
+		}
+	}
 };
 
 exports.createViews = function(db) {
@@ -85,6 +84,8 @@ exports.createViews = function(db) {
 exports.viewsAreUpToDate = function(db, callback) {
 	db.get(DESIGN_DOCUMENT_NAME, function (err, response) {
 		// if the design document does not exists, or the version # of the design doc does not exist, return false
+		callback(false);
+		return;
 		if(response == null || response.views.version == null) callback(false); 
 
 		else callback(response.views.version >= views.version);

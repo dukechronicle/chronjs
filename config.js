ddf15a03ce5389045e1402d2a97e7ed190144501
Joe Levy <jal50@duke.edu>
2011-08-07 18:13:19 -0700

    solr constants like url and port come from config file now

--- config.js
+++ config.js
@@ -7,6 +7,10 @@ var configuration = {
 			S3_KEY: 'AKIAIV2G6LCBMGKP35DQ',
 			S3_SECRET: '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',
 			REDIS_URL: 'redis://redistogo:0235bf0a2db5e6cc087683952f60c59c@icefish.redistogo.com:9249/',
+			SOLR_HOST: 'index.websolr.com',
+			SOLR_PORT: '80',
+			SOLR_CORE: '/c1af51aeb37',
+			SOLR_PATH: '/solr',
 		},
 		production: {
 			COUCHDB_URL: 'http://chrondev:pikachu@chrondev.iriscouch.com',
@@ -15,6 +19,10 @@ var configuration = {
 			S3_KEY: 'AKIAIV2G6LCBMGKP35DQ',
 			S3_SECRET: '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',
 			REDIS_URL: 'redis://redistogo:0235bf0a2db5e6cc087683952f60c59c@icefish.redistogo.com:9249/',
+			SOLR_HOST: 'index.websolr.com',
+			SOLR_PORT: '80',
+			SOLR_CORE: '/c1af51aeb37',
+			SOLR_PATH: '/solr',
 		}
 	},
 	activeConfigurationProfile: 'development'

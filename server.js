61eb87552b4f69a3aa40ea160b520741fa3ea6a8
Joe Levy <jal50@duke.edu>
2011-08-07 18:00:31 -0700

    Changed config from using redis to using a config file. Closes #64

--- server.js
+++ server.js
@@ -3,77 +3,75 @@ var PORT = process.env.PORT || 4000;
 
 var config = require('./thechronicle_modules/config');
 
-config.sync(function() {
-	/* require npm nodejs modules */
-	var express = require('express');
-	require('express-namespace');
-	var stylus = require('stylus');
-	
-	/* require internal nodejs modules */
-	var globalFunctions = require('./thechronicle_modules/global-functions');
-	var api = require('./thechronicle_modules/api/lib/api');
-	var site = require('./thechronicle_modules/api/lib/site');
-	var admin = require('./thechronicle_modules/admin/lib/admin');
-    var mobileapi = require('./thechronicle_modules/mobileapi/lib/mobileapi');
-	/* express configuration */
-	var app = express.createServer();
+/* require npm nodejs modules */
+var express = require('express');
+require('express-namespace');
+var stylus = require('stylus');
 
-	var publicDir = '/public';
-	var viewsDir = '/views';
+/* require internal nodejs modules */
+var globalFunctions = require('./thechronicle_modules/global-functions');
+var api = require('./thechronicle_modules/api/lib/api');
+var site = require('./thechronicle_modules/api/lib/site');
+var admin = require('./thechronicle_modules/admin/lib/admin');
+var mobileapi = require('./thechronicle_modules/mobileapi/lib/mobileapi');
+/* express configuration */
+var app = express.createServer();
 
-	function compile(str, path) {
-	  return stylus(str)
-		.set('filename', path)
-		.set('compress', true);
-	}
-	
-	// add the stylus middleware, which re-compiles when
-	// a stylesheet has changed, compiling FROM src,
-	// TO dest. dest is optional, defaulting to src
+var publicDir = '/public';
+var viewsDir = '/views';
 
-	app.use(stylus.middleware({
-		src: __dirname + viewsDir
-	  , dest: __dirname + publicDir
-	  , compile: compile
-	}));
+function compile(str, path) {
+  return stylus(str)
+	.set('filename', path)
+	.set('compress', true);
+}
 
-	app.set('view engine', 'jade');
+// add the stylus middleware, which re-compiles when
+// a stylesheet has changed, compiling FROM src,
+// TO dest. dest is optional, defaulting to src
 
-	// the middleware itself does not serve the static
-	// css files, so we need to expose them with staticProvider
-	app.use(express.static(__dirname + publicDir));
-	app.use(express.bodyParser());
+app.use(stylus.middleware({
+	src: __dirname + viewsDir
+  , dest: __dirname + publicDir
+  , compile: compile
+}));
 
-	app.set('views', __dirname + viewsDir);
+app.set('view engine', 'jade');
 
-	app.error(function(err, req, res, next){
-		res.send(500);
-		globalFunctions.log('ERROR: ' + err.stack);
-	});
+// the middleware itself does not serve the static
+// css files, so we need to expose them with staticProvider
+app.use(express.static(__dirname + publicDir));
+app.use(express.bodyParser());
 
-	/*** FRONTEND ***/
-	app.get('/', site.renderRoot);
-	app.get('/article-list', site.renderArticleList);
-	app.get('/article-list/:titleSearch', function(req, http_res) {
-		site.renderArticleListSearch(req, http_res, req.params.titleSearch);
-	});
-	app.get('/article/:url', function(req, http_res) {
-	    site.renderArticle(req, http_res, req.params.url);
-	});
-	app.get('/article/:url/edit', function(req, http_res) {
-	    site.renderArticleEdit(req, http_res, req.params.url);
-	});
-	app.get('/article/:url/image', function(req, http_res) {
-	    site.renderImageList(req, http_res, req.params.url);
-	});
-	/*** !FRONTEND ***/
-	
-	/*** ADMIN ***/
-	app = admin.init(app);
-    app = mobileapi.init(app);
+app.set('views', __dirname + viewsDir);
 
-	/*** !ADMIN ***/
-
-	console.log('Listening on port ' + PORT);
-	app.listen(PORT);
+app.error(function(err, req, res, next){
+	res.send(500);
+	globalFunctions.log('ERROR: ' + err.stack);
 });
+
+/*** FRONTEND ***/
+app.get('/', site.renderRoot);
+app.get('/article-list', site.renderArticleList);
+app.get('/article-list/:titleSearch', function(req, http_res) {
+	site.renderArticleListSearch(req, http_res, req.params.titleSearch);
+});
+app.get('/article/:url', function(req, http_res) {
+    site.renderArticle(req, http_res, req.params.url);
+});
+app.get('/article/:url/edit', function(req, http_res) {
+    site.renderArticleEdit(req, http_res, req.params.url);
+});
+app.get('/article/:url/image', function(req, http_res) {
+    site.renderImageList(req, http_res, req.params.url);
+});
+/*** !FRONTEND ***/
+
+/*** ADMIN ***/
+app = admin.init(app);
+app = mobileapi.init(app);
+
+/*** !ADMIN ***/
+
+console.log('Listening on port ' + PORT);
+app.listen(PORT);
--- thechronicle_modules/config/lib/config.js
+++ thechronicle_modules/config/lib/config.js
@@ -1,95 +1,15 @@
-var redis = require("redis");
-var url = require("url");
+var configFile = require('../../../config.js');
+var globalFunctions = require('../../global-functions');
 
-var configProfile = "dev";
-var configHashKey = "config:" + configProfile;
+var configuration = configFile.getConfiguration();
+var activeProfile = configuration.profiles[configuration.activeConfigurationProfile];
 
-// parse enviroment variable to extract login information
-var redisUrl = process.env.REDISTOGO_URL || process.env.REDIS_URL;
-
-if (redisUrl) {
-	redisUrl = url.parse(redisUrl);
-	redisUrl.auth = redisUrl.auth.split(":");
-
-	// create redis client and authenticate
-	var client = redis.createClient(redisUrl.port, redisUrl.hostname);
-	client.auth(redisUrl.auth[1], function(err, reply) {
-		if (err) {
-			throw "Error connecting to redis: " + err;
-		}
-	});
-	client.on("error", function (err) {
-        console.log("Error " + err);
-	});
-
-} else {
-	console.log("WARNING: REDIS_URL is not set, configurations may be missing");
-	client = null;
+if(activeProfile == null) {
+	if(configuration.activeConfigurationProfile == null) globalFunctions.log('Active configuration profile is not defined!');
+	else globalFunctions.log('Configuration profile: "' + configuration.activeConfigurationProfile + '" does not exist!');
 }
 
-var config = exports;
-var configCache = null;
-
-// syncronizes the live redis config to local configCache variable so config.get does not need to be async
-config.sync = function(callback) {
-	if (client) {
-		client.HGETALL(configHashKey, function(err, obj) {
-			if (err) throw err;
-
-			if (configCache === null) {
-				console.log(configProfile + " configuration:");
-				// show overwritten configs
-				var displayConfig = {};
-				Object.keys(obj).forEach(function(key) {
-					if (process.env[key]) {
-						displayConfig["**" + key + "**"] = process.env[key];
-					} else {
-						displayConfig[key] = obj[key];
-					}
-				});
-				console.log(JSON.stringify(displayConfig, null, "\t"));
-			}
-			configCache = obj;
-
-			if (typeof callback !== "undefined") {
-				callback(err, obj);
-			}
-		});
-	} else {
-		if (configCache === null) configCache = {};
-		callback();
-	}
-};
-
-// set a configuration variable to the given value
-// we set the local cache first so config.get does not have to wait for redis set operation to complete
-config.set = function(variable, value) {
-	configCache[variable] = value;
-	if (client) {
-		client.HSET(configHashKey, variable, value, function(err, result) {
-			if (err) throw err;
-		});
-	}
-};
-
-config.del = function(variable) {
-	delete configCache[variable];
-	if (client) {
-		client.HDEL(configHashKey, variable);
-	}
-};
-
-config.get = function(variable, defaultValue) {
-	// first check envrionment var, then redis, then defaultValue
-	if (process.env[variable]) {
-		return process.env[variable];
-	} else if (configCache[variable]) {
-		return configCache[variable];
-	} else {
-		return defaultValue;
-	}
-};
-
-config.getAll = function() {
-	return configCache;
+exports.get = function(variable) {
+	if(activeProfile[variable] == null) globalFunctions.log('Configuration property: "' + variable + '" not defined!');
+	return activeProfile[variable];
 };

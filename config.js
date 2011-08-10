0cca182abf87d2caf4c7e6ef826d7125bdfbfb27
Joe Levy <jal50@duke.edu>
2011-08-10 00:58:42 -0700

    web portal to configure chronicle framework upon first server boot up. Also includes easy way to add new configuration parameters to the portal through a json file in the config folder

--- config.js
+++ config.js
@@ -1,34 +1,9 @@
-var configuration = {
-	profiles: {
-		development: {
-			COUCHDB_URL: 'http://chrondev:pikachu@chrondev.iriscouch.com',
-			COUCHDB_DATABASE: 'chronicle_dev',
-			S3_BUCKET: 'chron_bucket1',
-			S3_KEY: 'AKIAIV2G6LCBMGKP35DQ',
-			S3_SECRET: '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',
-			REDIS_URL: 'redis://redistogo:0235bf0a2db5e6cc087683952f60c59c@icefish.redistogo.com:9249/',
-			SOLR_HOST: 'index.websolr.com',
-			SOLR_PORT: '80',
-			SOLR_CORE: '/c1af51aeb37',
-			SOLR_PATH: '/solr',
-		},
-		production: {
-			COUCHDB_URL: 'http://chrondev:pikachu@chrondev.iriscouch.com',
-			COUCHDB_DATABASE: 'production',
-			S3_BUCKET: 'chron_bucket1',
-			S3_KEY: 'AKIAIV2G6LCBMGKP35DQ',
-			S3_SECRET: '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',
-			REDIS_URL: 'redis://redistogo:0235bf0a2db5e6cc087683952f60c59c@icefish.redistogo.com:9249/',
-			SOLR_HOST: 'index.websolr.com',
-			SOLR_PORT: '80',
-			SOLR_CORE: '/c1af51aeb37',
-			SOLR_PATH: '/solr',
-		}
-	},
-	activeConfigurationProfile: 'development'
-};
-
 exports.getConfiguration = function()
 {
-	return configuration;
+	try {
+		return configuration;
+	}
+	catch(err) {
+		return null;
+	}
 }
--- server.js
+++ server.js
@@ -1,19 +1,15 @@
 /* declare global configuration variables */
 var PORT = process.env.PORT || 4000;
 
-var config = require('./thechronicle_modules/config');
-
 /* require npm nodejs modules */
 var express = require('express');
 require('express-namespace');
 var stylus = require('stylus');
 
-/* require internal nodejs modules */
+/* require internal modules */
 var globalFunctions = require('./thechronicle_modules/global-functions');
-var api = require('./thechronicle_modules/api/lib/api');
-var site = require('./thechronicle_modules/api/lib/site');
-var admin = require('./thechronicle_modules/admin/lib/admin');
-var mobileapi = require('./thechronicle_modules/mobileapi/lib/mobileapi');
+var config = require('./thechronicle_modules/config');
+
 /* express configuration */
 var app = express.createServer();
 
@@ -35,7 +31,6 @@ app.use(stylus.middleware({
   , dest: __dirname + publicDir
   , compile: compile
 }));
-
 app.set('view engine', 'jade');
 
 // the middleware itself does not serve the static
@@ -50,28 +45,54 @@ app.error(function(err, req, res, next){
 	globalFunctions.log('ERROR: ' + err.stack);
 });
 
-/*** FRONTEND ***/
-app.get('/', site.renderRoot);
-app.get('/article-list', site.renderArticleList);
-app.get('/article-list/:titleSearch', function(req, http_res) {
-	site.renderArticleListSearch(req, http_res, req.params.titleSearch);
-});
-app.get('/article/:url', function(req, http_res) {
-    site.renderArticle(req, http_res, req.params.url);
-});
-app.get('/article/:url/edit', function(req, http_res) {
-    site.renderArticleEdit(req, http_res, req.params.url);
-});
-app.get('/article/:url/image', function(req, http_res) {
-    site.renderImageList(req, http_res, req.params.url);
-});
-/*** !FRONTEND ***/
+if(!config.isSetUp())
+{
+	app.get('/', function(req, res, next) {
+		if(!config.isSetUp()) res.render('admin/config', {locals:{configParams:config.getParameters()}, layout: 'layout-admin.jade'} );
+		else next();
+	});
 
-/*** ADMIN ***/
-app = admin.init(app);
-app = mobileapi.init(app);
-
-/*** !ADMIN ***/
+	app.post('/', function(req, res) {
+		config.setUp(req.body, function(err) {
+			if(err == null) runSite();
+			res.redirect('/');
+		});
+	});
+}
+else runSite();
 
 console.log('Listening on port ' + PORT);
 app.listen(PORT);
+
+function runSite()
+{
+	/* require internal nodejs modules */
+	var api = require('./thechronicle_modules/api/lib/api');
+	var site = require('./thechronicle_modules/api/lib/site');
+	var admin = require('./thechronicle_modules/admin/lib/admin');
+	var mobileapi = require('./thechronicle_modules/mobileapi/lib/mobileapi');
+	
+
+	/*** FRONTEND ***/
+	app.get('/', site.renderRoot);
+
+	app.get('/article-list', site.renderArticleList);
+	app.get('/article-list/:titleSearch', function(req, http_res) {
+		site.renderArticleListSearch(req, http_res, req.params.titleSearch);
+	});
+	app.get('/article/:url', function(req, http_res) {
+	    site.renderArticle(req, http_res, req.params.url);
+	});
+	app.get('/article/:url/edit', function(req, http_res) {
+	    site.renderArticleEdit(req, http_res, req.params.url);
+	});
+	app.get('/article/:url/image', function(req, http_res) {
+	    site.renderImageList(req, http_res, req.params.url);
+	});
+	/*** !FRONTEND ***/
+
+	/*** ADMIN ***/
+	app = admin.init(app);
+	app = mobileapi.init(app);
+	/*** !ADMIN ***/
+}

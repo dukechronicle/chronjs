34463032a9dc95299a4099e390633c68b053c120
Ethan Goh <yhgoh88@gmail.com>
2011-08-11 02:05:45 -0700

    Added article retrieving api call for mobile site.

--- thechronicle_modules/mobileapi/lib/mobileapi.js
+++ thechronicle_modules/mobileapi/lib/mobileapi.js
@@ -1,5 +1,6 @@
 var api = require('../../api/lib/api');
 var taxonomyGroups = ["News","Sports","Opinion","Recess","Towerview"];
+var _ = require('underscore');
 
 exports.init = function(app) {
 	app.namespace('/mobile', function() {
@@ -7,11 +8,19 @@ exports.init = function(app) {
             var groupName = req.params.groupname;
             //console.log("server.js/mobile" + groupName);
             getGroup(groupName,10,function(err,res){
-                console.log("site.js " + res);
-                for(doc in res)
-                {
-                    console.log(res[doc].value.title);
-                }
+                console.log(res.teaser);
+                var result = [];
+                result = _.map(res,function(doc){
+                    return {"title": doc.value.title, "teaser": doc.value.teaser, "urls": doc.value.urls};
+                });
+                http_res.send(result);
+            });
+        });
+
+        app.get('/article/:articleURL', function(req, http_res) {
+            var articleURL = req.params.articleURL;
+            api.docForUrl(articleURL, function(err,res){
+                //console.log(res);
                 http_res.send(res);
             });
         });
@@ -22,7 +31,6 @@ exports.init = function(app) {
 
 function grabArticles(groupName, baseDocNum, n,callback){
     api.taxonomy.docs(groupName[0],n,function(err,test){
-        console.log(test);
         if(err)
             callback(err,null);
         else

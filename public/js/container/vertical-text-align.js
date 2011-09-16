aae4850cafad62f5bb209a824bc1fdbf5cc657b7
Dean Chen <dean@ocirs.com>
2011-09-16 06:53:46 -0400

    updated vertical text align to automatically expand the height it's box to at least the same height as its label

--- public/js/container/vertical-text-align.js
+++ public/js/container/vertical-text-align.js
@@ -7,6 +7,11 @@ function verticalAlign() {
 		height = parseInt(height.substring(0, height.length - 2), 10) + topSpacing;
 		$(this).css('top', height + "px");
 		$(this).css('visibility', 'visible');
+
+        var rounded = $(this).siblings(".rounded");;
+        if (rounded.css('height').substring(0, rounded.css('height').length - 2) < height) {
+            rounded.css('height', height + "px");
+        }
 	});
 }
 

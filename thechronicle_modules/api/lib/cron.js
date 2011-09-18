b55e82de4c6ad01b707d9671df12e0d8f059b086
Alex Galonsky <alex@galonsky.com>
2011-09-18 19:30:53 -0400

    Fixed cron. Fixed sports feed url.

--- thechronicle_modules/api/lib/cron.js
+++ thechronicle_modules/api/lib/cron.js
@@ -3,17 +3,19 @@ var rss = require('./rss');
 
 var feeds = [{
     title: "sportsblog",
-    url: "http://sports.chronicleblogs.com/feed/"
+    url: "http://feeds.feedburner.com/chronicleblogs/sports"
 }];
 
 exports.init = function() {
     new cron.CronJob('0 0 * * * *', function() { //every hour?
         feeds.forEach(function(feed) {
             rss.parseRSS(feed.url, function(err, dom) {
+                console.log("Parsed RSS for feed: " + feed.title);
                 if(err) console.log(err);
                 else {
-                    rss.storeRSS(dom, title, function(err, res) {
+                    rss.storeRSS(dom, feed.title, function(err, res) {
                         if(err) console.log(err);
+                        else console.log("Stored RSS for feed: " + feed.title);
                     });
                 }
             });

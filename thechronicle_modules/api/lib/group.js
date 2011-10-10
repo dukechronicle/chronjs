cea7974814ec56cd8fd1fa8828fe0616e15f3f70
Dean Chen <dean@ocirs.com>
2011-10-10 02:17:09 -0400

    increased group cache to 120 seconds

--- thechronicle_modules/api/lib/group.js
+++ thechronicle_modules/api/lib/group.js
@@ -128,7 +128,7 @@ group.docs = function(namespace, group, callback) {
                                 groupedResults[prevGroupName][groupedResults[prevGroupName].length - 1].cssClass = "last";
                             }
                             redis.client.set(key, JSON.stringify(groupedResults));
-                            redis.client.expire(key, 60);
+                            redis.client.expire(key, 120);
                             callback(null, groupedResults);
                         } else {
                             // TODO modify this

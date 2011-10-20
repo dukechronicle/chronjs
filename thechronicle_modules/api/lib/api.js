e0954d6cc1663039c429df115ccb0d4072753e02
Dean Chen <dean@ocirs.com>
2011-10-20 08:18:40 -0400

    disabled continous indexing

--- thechronicle_modules/api/lib/api.js
+++ thechronicle_modules/api/lib/api.js
@@ -70,12 +70,12 @@ api.init = function(callback) {
         api.search.init();
         //api.search.indexUnindexedArticles(1);
         /** Chron Jobs! **/
-
+        /*
         new cron.CronJob('0 * * * * *', function() {
             process.nextTick(function() {
                 api.search.indexUnindexedArticles(300);
             });
-        });
+        });*/
 
         callback(null);
     });

b3d287ce236ddc8dde46a72e27b0d2dc964b818d
Jim Posen <jim.posen@gmail.com>
2011-10-31 10:19:20 -0400

    Fixed author shown as array bug with k4export.

--- public/js/admin/k4export.js
+++ public/js/admin/k4export.js
@@ -1,7 +1,7 @@
 function editDocument(article) {
     $("#sub"+article.id).attr('disabled', 'disabled');
     article.taxonomy = JSON.stringify($("#tax"+article.id).val().split(","));
-    article.authors = JSON.stringify(article.authors);
+    article.authors = article.authors.toString();
     $.post('/admin/edit', { doc: article }, function(data, status) {
 	if (status != 'success')
 	    alert("Taxonomy change for article '" + article.title + "' failed");

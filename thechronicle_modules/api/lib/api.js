c7fb5c3b452527fcd2bdd3e174163db2e5bc9901
Dean Chen <dean@ocirs.com>
2011-10-31 12:10:10 -0400

    default to allow stale view results in api

--- thechronicle_modules/api/lib/api.js
+++ thechronicle_modules/api/lib/api.js
@@ -19,6 +19,10 @@ var redis = require('./redisclient');
 
 var MAX_URL_LENGTH = 50;
 
+var queryDefaults = {
+    stale: "ok"
+}
+
 function getAvailableUrl(url, n, callback) {
     var new_url = url;
     if(n != 0) {
@@ -160,7 +164,7 @@ api.docsByAuthor = function(author, callback) {
 
     var query = {descending: true, startkey:decodeAuthor, endkey: decodeAuthor};
 
-    db.view("articles/authors", query, function(err, results) {
+    db.view("articles/authors", _.defaults(query, queryDefaults), function(err, results) {
         if (err)
         {
             callback(err);
@@ -219,7 +223,7 @@ api.articleForUrl = function(url, callback) {
         limit: 20
     };
 
-    db.view("articles/urls", query, function(err, docs) {
+    db.view("articles/urls", _.defaults(query, queryDefaults), function(err, docs) {
 
         if (err) return callback(err);
         var docTypeKey = 1;
@@ -252,7 +256,7 @@ api.docForUrl = function(url, callback) {
         limit: 20
     };
 
-    db.view("articles/urls", query, function(err, docs) {
+    db.view("articles/urls", _.defaults(query, queryDefaults), function(err, docs) {
         
         if (err) return callback(err);
         var docTypeKey = 1;
@@ -292,7 +296,7 @@ api.docsByDate = function(limit, callback) {
         query.limit = 20;
     }
     
-    db.view("articles/all_by_date", query, function(err, results) {
+    db.view("articles/all_by_date", _.defaults(query, queryDefaults), function(err, results) {
         if (err) callback(err);
 
         // return only the array of the result values

8ee403ab681d0548cc26a631b1b1c8563ac65ebf
Lindsay Kubasik <lindsay.kubasik@gmail.com>
2011-11-12 17:46:34 -0500

    created init function for logger so that it wont break if config not set at server start

--- thechronicle_modules/log/lib/log.js
+++ thechronicle_modules/log/lib/log.js
@@ -6,20 +6,23 @@ var _ = require('underscore');
 var logger = new (winston.Logger)();
 _.extend(exports, logger);
 
+exports.init = function() {
+	logger.setLevels(winston.config.syslog.levels);
+	logger.add(winston.transports.Console, { level: 'debug' });
 
-logger.setLevels(winston.config.syslog.levels);
-logger.add(winston.transports.Console, { level: 'debug' });
-if (process.env.NODE_ENV === 'production')
-    logger.add(winston.transports.Loggly, {
-	subdomain: config.get('LOGGLY_SUBDOMAIN'),
-	inputToken: config.get('LOGGLY_INPUT_KEY'),
-	level: 'warning',
-	json: true
-    });
-logger.handleExceptions();
-logger.warning('Logger is up');
+	if (process.env.NODE_ENV === 'production')
+    	logger.add(winston.transports.Loggly, {
+		subdomain: config.get('LOGGLY_SUBDOMAIN'),
+		inputToken: config.get('LOGGLY_INPUT_KEY'),
+		level: 'warning',
+		json: true
+    	});
+    
+	logger.handleExceptions();
+	logger.warning('Logger is up');
 
-// TODO: Handle logging errors with email alert
-logger.on('error', function(err) {
-    console.error("Logging error: " + JSON.stringify(err));
-});
+	// TODO: Handle logging errors with email alert
+	logger.on('error', function(err) {
+    	console.error("Logging error: " + JSON.stringify(err));
+	});	
+};

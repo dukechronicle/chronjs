e3ba6fcaeee6db7c9de186ea10d7f01c1f742ec9
Jim Posen <jim.posen@gmail.com>
2011-11-12 22:12:42 -0500

    Made logger log exceptions on console.

--- thechronicle_modules/log/lib/log.js
+++ thechronicle_modules/log/lib/log.js
@@ -7,22 +7,26 @@ var logger = new (winston.Logger)();
 _.extend(exports, logger);
 
 exports.init = function() {
-	logger.setLevels(winston.config.syslog.levels);
-	logger.add(winston.transports.Console, { level: 'debug' });
+    logger.setLevels(winston.config.syslog.levels);
+    logger.add(winston.transports.Console, {
+	level: 'debug',
+	handleExceptions: true
+    });
 
-	if (process.env.NODE_ENV === 'production')
+    if (process.env.NODE_ENV === 'production')
     	logger.add(winston.transports.Loggly, {
-		subdomain: config.get('LOGGLY_SUBDOMAIN'),
-		inputToken: config.get('LOGGLY_INPUT_KEY'),
-		level: 'warning',
-		json: true
+	    subdomain: config.get('LOGGLY_SUBDOMAIN'),
+	    inputToken: config.get('LOGGLY_INPUT_KEY'),
+	    level: 'warning',
+	    json: true,
+	    handleExceptions: true
     	});
     
-	logger.handleExceptions();
-	logger.warning('Logger is up');
+    logger.handleExceptions();
+    logger.warning('Logger is up');
 
-	// TODO: Handle logging errors with email alert
-	logger.on('error', function(err) {
+    // TODO: Handle logging errors with email alert
+    logger.on('error', function(err) {
     	console.error("Logging error: " + JSON.stringify(err));
-	});	
+    });	
 };

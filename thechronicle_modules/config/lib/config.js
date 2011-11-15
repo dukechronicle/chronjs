c049f22f5d0e167ae42c10abe507ca4751bc2129
Dean Chen <dean@ocirs.com>
2011-11-14 22:58:05 -0500

    refactored config.js, most importantly the removal of declaration of global vars

--- thechronicle_modules/config/lib/config.js
+++ thechronicle_modules/config/lib/config.js
@@ -42,7 +42,7 @@ function getConfigParamObjectWithName(name) {
     var params = configParams.getParameters();
 
     return _.find(params, function(index) {
-        params[index].name === name;
+        return params[index].name === name;
     });
 }
 
@@ -58,77 +58,76 @@ exports.get = function(variable) {
     return activeProfile[variable];
 };
 
-exports.isSetUp = function() {
+exports.isSetUp = function () {
     return exports.getUndefinedParameters().length == 0; // if all config params are defined, it is set up
-}
+};
 
-exports.setUp = function(params, callback) {
-    var addToConfigFile = "exports.getConfiguration = function(){try {return configuration;}catch(err) {return null;}}";     
-    
-    // remove configuration profile name from parameter set as it is not a configuration parameter    
+exports.setUp = function (params, callback) {
+    var addToConfigFile = "exports.getConfiguration = function(){try {return configuration;}catch(err) {return null;}}";
+
+    // remove configuration profile name from parameter set as it is not a configuration parameter
     var profileName = params[PROFILE_NAME_KEY];
     delete params[PROFILE_NAME_KEY];
 
     // build the configuration object if needed
-    if(configuration == null) configuration = {};
-    if(configuration.profiles == null) configuration.profiles = {};
-    if(configuration.profiles[profileName] == null)    configuration.profiles[profileName] = {};
-    
-    configuration.activeConfigurationProfile = profileName;    
+    if (configuration == null) configuration = {};
+    if (configuration.profiles == null) configuration.profiles = {};
+    if (configuration.profiles[profileName] == null)    configuration.profiles[profileName] = {};
 
-    for(configKey in params) {
-        if(params[configKey].length > 0) {
-            configuration.profiles[configuration.activeConfigurationProfile][configKey] = params[configKey];
+    configuration.activeConfigurationProfile = profileName;
+
+    Object.keys(params).forEach(function (key) {
+        if (params[key].length > 0) {
+            configuration.profiles[configuration.activeConfigurationProfile][key] = params[key];
         }
-    }
-    
+    });
+
     activeProfileName = configuration.activeConfigurationProfile;
     activeProfile = configuration.profiles[activeProfileName];
 
     // write the config file
-    var writeToFile = 'var configuration = \n'+ JSON.stringify(configuration, null, 4) + ';\n\n' + addToConfigFile;
-    fs.writeFile(CONFIG_FILE_PATH, writeToFile, function(err) {
-        if(exports.getUndefinedParameters().length == 0) callback(null);
-        else callback('some parameters still undefined');
+    var writeToFile = 'var configuration = \n' + JSON.stringify(configuration, null, 4) + ';\n\n' + addToConfigFile;
+    fs.writeFile(CONFIG_FILE_PATH, writeToFile, function (err) {
+        if (err) return callback(err);
+        if (exports.getUndefinedParameters().length == 0) return callback(null);
+        else return callback('some parameters still undefined');
     });
-}
+};
 
-exports.getUndefinedParameters = function() {
-    if(configuration == null) return configParams.getParameters();
+exports.getUndefinedParameters = function () {
+    if (configuration == null) return configParams.getParameters();
 
     var parameters = configParams.getParameters();
-    var returnParameters = [];
 
     // find the undefined params and return them
-    for(i = 0; i < parameters.length; i ++) {
-        // if a parameter is undefined, add it to return array
-        if(activeProfile[parameters[i].name] == null) returnParameters.push(parameters[i]); 
-    }
-    log.warning("Undefined parameters: " + JSON.stringify(returnParameters));
-    return returnParameters;
-}
+    var undefinedParameters = _.filter(parameters, function (parameter) {
+        return activeProfile[parameter.name] == null;
+    });
+
+    if (undefinedParameters.length > 0) log.warning("Undefined parameters: " + JSON.stringify(undefinedParameters));
+    return undefinedParameters;
+};
 
 exports.getParameters = function () {
     if(configuration == null) return configParams.getParameters();
     
     var returnParams = exports.getUndefinedParameters();
-    
 
-    for(var key in activeProfile) {
-        var newobj = {};
-        newobj.name = key;
-        newobj.description = getConfigParamObjectWithName(key).description;
-        newobj.defaultValue = activeProfile[key];
-        returnParams.push(newobj);
-    }
+    Object.keys(activeProfile).forEach(function(key) {
+        var defaultParameter = {};
+        defaultParameter.name = key;
+        defaultParameter.description = getConfigParamObjectWithName(key).description;
+        defaultParameter.defaultValue = activeProfile[key];
+        returnParams.push(defaultParameter);
+    });
 
     return returnParams;
-}
+};
 
 exports.getActiveProfileName = function() {
     return activeProfileName || DEFAULT_PROFILE_NAME;
-}
+};
 
 exports.getProfileNameKey = function() {
     return PROFILE_NAME_KEY;
-}
+};

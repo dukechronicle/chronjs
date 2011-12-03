5747160ea65c9bcd886f639a968fd6e37c98bbad
Dean Chen <dean.chen@duke.edu>
2011-12-02 20:20:30 -0500

    styled title font on slideshow

--- public/js/slideshow/frontpage-slideshow-init.js
+++ public/js/slideshow/frontpage-slideshow-init.js
@@ -1,8 +1,9 @@
 $(function() {
     Galleria.loadTheme('/js/galleria/themes/frontpage-slideshow/galleria.frontpage-slideshow.js');
     $("#slides").galleria({
-            transition: "fade",
-            width: 636,
-            height: 393
+        transition: "fade",
+        transitionSpeed: 900,
+        width: 636,
+        height: 393
     });
 });

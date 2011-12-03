19a2e223ba120c4e78fa372df92191d52cca96cc
Dean Chen <dean.chen@duke.edu>
2011-12-02 19:10:04 -0500

    added galleria for frontpage slideshow

--- config.js
+++ config.js
@@ -1,5 +1,119 @@
 var configuration = {
     "profiles": {
+        "production": {
+            "COUCHDB_URL":"https://app578498.heroku:NNbL2x3Bu5vGLgComPjWxxET@app578498.heroku.cloudant.com",
+            "COUCHDB_DATABASE":"production",
+
+            "SERVER_PORT":"4000",
+
+            "ADMIN_USERNAME":"dean",
+            "ADMIN_PASSWORD":"dean",
+
+            "REDIS_URL":"redis://redistogo:0235bf0a2db5e6cc087683952f60c59c@icefish.redistogo.com:9249/",
+
+            "SOLR_HOST":"index.websolr.com",
+            "SOLR_PORT":"80",
+            "SOLR_PATH":"/solr",
+            "SOLR_CORE":"/43ff5b432fb",
+
+            "S3_KEY":"AKIAJIH3MOVTAMXCFKXA",
+            "S3_SECRET":"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",
+            "S3_BUCKET":"chron_bucket1",
+
+            "MAILCHIMP_API_KEY":"740856b1876fd04723d34bd00aa381d3-us2",
+            "MAILCHIMP_LIST_ID":"bc302eeb8d",
+            "MAILCHIMP_TEMPLATE_ID":"233513",
+
+            "TAXONOMY_MAIN_SECTIONS":["News","Sports", "Opinion", "Recess", "Towerview"],
+            "TAXONOMY_BAD_SECTIONS":[
+                'News/Graduation Issue',
+                'Sports/Tennis','Sports/Basketball',
+                'Sports/Soccer','Sports/Golf',
+                'Sports/Lacross', 'Sports/Sports Briefs',
+                'Sports/Olympics', 'Sports/Sports Features',
+                'Sports/Sports Column', 'Sports/Department of Athletics',
+                'Sports/Durham Bulls', 'Sports/Wrestling',
+                'Sports/Volleyball', 'Sports/Fencing',
+                'Sports/Field Hockey',
+                'Opinion/Cartoons', 'Towerview/Blue Devil Crossing',
+                'Towerview/Bus Stop',
+                'Towerview/Blue Devil Crossing',
+                'Towerview/Driving Distance',
+                'Towerview/Rearview Mirror',
+                'Towerview/The Devil\'s Details',
+                'Towerview/The Green Light'
+            ],
+
+            "LAYOUT_GROUPS":{
+                "Frontpage": {
+                    "namespace": ['Layouts','Frontpage'],
+                    "groups": ["Slideshow","Left Headlines","Right Headlines","Opinion","News","Sports","Recess","Towerview"]
+                },
+                "News": {
+                    "namespace": ['Layouts','News'],
+                    "groups": ["Featured", "Right Headlines", "Headlines", "Stories"]
+                },
+                "Sports": {
+                    "namespace": ['Layouts','Sports'],
+                    "groups": ["Slideshow", "Stories"]
+                },
+                "Opinion": {
+                    "namespace": ['Layouts','Opinion'],
+                    "groups": ["Featured","Columnists","Edit Board","More Columnists"]
+                },
+                "Recess": {
+                    "namespace": ['Layouts','Recess'],
+                    "groups": ["Featured","Sandbox","Interviews","Reviews","Stories"]
+                },
+                "Towerview": {
+                    "namespace": ['Layouts','Towerview'],
+                    "groups": ["Featured","Savvy","Wisdom","Editors Note","Prefix"]
+                }
+            },
+
+            "RSS_FEEDS":[
+                {
+                    title: "sportsblog",
+                    url: "http://feeds.feedburner.com/chronicleblogs/sports"
+                },
+                {
+                    title: "twitter-DukeChronicle",
+                    url: "http://api.twitter.com/1/statuses/user_timeline.rss?screen_name=DukeChronicle"
+                },
+                {
+                    title: "twitter-ChronicleRecess",
+                    url: "http://api.twitter.com/1/statuses/user_timeline.rss?screen_name=ChronicleRecess"
+                },
+                {
+                    title: "twitter-TowerviewMag",
+                    url: "http://api.twitter.com/1/statuses/user_timeline.rss?screen_name=TowerviewMag"
+                },
+                {
+                    title: "twitter-DukeBasketball",
+                    url: "http://api.twitter.com/1/statuses/user_timeline.rss?screen_name=dukebasketball"
+                },
+                {
+                    title: "twitter-ChronPhoto",
+                    url: "http://api.twitter.com/1/statuses/user_timeline.rss?screen_name=ChronPhoto"
+                },
+                {
+                    title: "twitter-ChronicleSports",
+                    url: "http://api.twitter.com/1/statuses/user_timeline.rss?screen_name=chroniclesports"
+                },
+                {
+                    title: "newsblog",
+                    url: "http://feeds.feedburner.com/chronicleblogs/news"
+                },
+                {
+                    title: "recessblog",
+                    url: "http://feeds.feedburner.com/chronicleblogs/playground"
+                },
+                {
+                    title: "blog-opinion",
+                    url: "http://feeds.feedburner.com/chronicleblogs/backpages"
+                }
+            ]
+        },
         "dev": {
             
             "COUCHDB_URL":"https://chrondev:pikachu@chrondev.iriscouch.com",
@@ -116,7 +230,7 @@ var configuration = {
             ]
         }
     },
-    "activeConfigurationProfile":"dev"
+    "activeConfigurationProfile":"production"
 };
 
 exports.getConfiguration = function() {

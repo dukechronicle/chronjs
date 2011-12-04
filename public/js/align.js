1e505f10c0a7aa5f53f3737076e280f5c902add0
Dean Chen <dean.chen@duke.edu>
2011-12-04 00:57:53 -0500

    only run frontpage align code when on the frontpage

--- public/js/align.js
+++ public/js/align.js
@@ -33,32 +33,33 @@ define(["jquery", "underscore"], function($) {
                 });
             });
 
+
+        },
+        frontpageAlign: function() {
             // align main and sidebar height
-            (function () {
-                var popularLiHeight = 24;
+            var popularLiHeight = 24;
 
-                var extraHeight = $('#top > .sidebar').height() - $('#top > .content').height() + 3;
-                var contentContainer = $('#top > .content .top-news .content-container');
-                var popularContainer = $('#top > .sidebar .most-popular .content-container');
+            var extraHeight = $('#top > .sidebar').height() - $('#top > .content').height() + 3;
+            var contentContainer = $('#top > .content .top-news .content-container');
+            var popularContainer = $('#top > .sidebar .most-popular .content-container');
 
-                console.log(extraHeight);
-                if (extraHeight > 0) {
-                    var lisToRemove = Math.floor(extraHeight / popularLiHeight);
-                    console.log(lisToRemove);
+            //console.log(extraHeight);
+            if (extraHeight > 0) {
+                var lisToRemove = Math.floor(extraHeight / popularLiHeight);
+                //console.log(lisToRemove);
 
-                    var removeIndex = $("li", popularContainer).size() - lisToRemove - 1;
-                    if (removeIndex < 2) {
-                        removeIndex = 2;
-                        lisToRemove = $("li", popularContainer).size() - removeIndex - 1;
-                    }
-                    console.log(removeIndex);
-                    $("li:gt(" + removeIndex + ")", popularContainer).hide();
-                    extraHeight -= lisToRemove * popularLiHeight;
+                var removeIndex = $("li", popularContainer).size() - lisToRemove - 1;
+                if (removeIndex < 2) {
+                    removeIndex = 2;
+                    lisToRemove = $("li", popularContainer).size() - removeIndex - 1;
                 }
-                console.log("extra height: " + extraHeight);
-                contentContainer.css('padding-bottom', extraHeight);
-                console.log($('#top > .sidebar').height() - $('#top > .content').height());
-            })();
+                //console.log(removeIndex);
+                $("li:gt(" + removeIndex + ")", popularContainer).hide();
+                extraHeight -= lisToRemove * popularLiHeight;
+            }
+            //console.log("extra height: " + extraHeight);
+            contentContainer.css('padding-bottom', extraHeight);
+            //console.log($('#top > .sidebar').height() - $('#top > .content').height());
         },
         verticalAlign: function() {
             // position vertical labels

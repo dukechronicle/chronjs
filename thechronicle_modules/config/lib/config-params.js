ff01f5c94c36f5cfbc91cc739e7e9e1e10aae9fb
Joe Levy <jal50@duke.edu>
2011-12-13 04:40:36 -0500

    filled out all non-secure default values in config defaults

--- thechronicle_modules/config/lib/config-params.js
+++ thechronicle_modules/config/lib/config-params.js
@@ -7,12 +7,12 @@ var configParams = [
     {
         name: 'ADMIN_USERNAME',
         description: 'New administrator username to login with from now on',
-        defaultValue: ''
+        defaultValue: 'admin'
     },
     {
         name: 'ADMIN_PASSWORD',
         description: 'New administrator password to login with from now on',
-        defaultValue: ''
+        defaultValue: 'chronicle    '
     },
     {
         name: 'COUCHDB_URL',
@@ -42,7 +42,7 @@ var configParams = [
     {
         name: 'REDIS_URL',
         description: 'Redis Server URL',
-        defaultValue: ''
+        defaultValue: 'redis://jodoglevy:2b258cbdcbbce003452a0ff4279d8701@barreleye.redistogo.com:9148/'
     },
     {
         name: 'SOLR_HOST',
@@ -57,7 +57,7 @@ var configParams = [
     {
         name: 'SOLR_CORE',
         description: 'WebSolr Core',
-        defaultValue: ''
+        defaultValue: '/3f534ff3ff0'
     },
     {
         name: 'SOLR_PATH',
@@ -67,22 +67,327 @@ var configParams = [
     {
         name: 'TAXONOMY_MAIN_SECTIONS',
         description: 'Main sections of the taxonomy',
-        defaultValue: {}
+        defaultValue: [
+            "News",
+            "Sports",
+            "Opinion",
+            "Recess",
+            "Towerview"
+        ]
     },
     {
         name: 'TAXONOMY',
         description: 'Taxonomy tree for the site, used for categorizing articles and navigation',
-        defaultValue: {}
+        defaultValue: [
+                {
+                    "News": [
+                        {
+                            "University": [
+                                {
+                                    "Residence Life": []
+                                },
+                                {
+                                    "Dining": []
+                                },
+                                {
+                                    "DSG": []
+                                },
+                                {
+                                    "Graduate and Professional Schools": []
+                                },
+                                {
+                                    "Student Life": []
+                                },
+                                {
+                                    "Campus Crime": []
+                                },
+                                {
+                                    "Academics": []
+                                },
+                                {
+                                    "Board of Trustees": []
+                                }
+                            ]
+                        },
+                        {
+                            "Local & National": [
+                                {
+                                    "City of Durham": []
+                                },
+                                {
+                                    "Higher Education": []
+                                },
+                                {
+                                    "Durham Crime": []
+                                },
+                                {
+                                    "North Carolina": []
+                                },
+                                {
+                                    "National News and Politics": []
+                                }
+                            ]
+                        },
+                        {
+                            "Health & Science": [
+                                {
+                                    "Research": []
+                                },
+                                {
+                                    "Pratt": []
+                                },
+                                {
+                                    "DUHS": []
+                                },
+                                {
+                                    "Environment & Sustainability": []
+                                },
+                                {
+                                    "Student Health": []
+                                },
+                                {
+                                    "School of Medicine": []
+                                }
+                            ]
+                        }
+                    ]
+                },
+                {
+                    "Sports": [
+                        {
+                            "Men": [
+                                {
+                                    "Baseball": []
+                                },
+                                {
+                                    "Basketball": []
+                                },
+                                {
+                                    "Cross country": []
+                                },
+                                {
+                                    "Fencing": []
+                                },
+                                {
+                                    "Football": []
+                                },
+                                {
+                                    "Golf": []
+                                },
+                                {
+                                    "Lacrosse": []
+                                },
+                                {
+                                    "Soccer": []
+                                },
+                                {
+                                    "Swimming and diving": []
+                                },
+                                {
+                                    "Tennis": []
+                                },
+                                {
+                                    "Track and field": []
+                                },
+                                {
+                                    "Wrestling": []
+                                }
+                            ]
+                        },
+                        {
+                            "Women": [
+                                {
+                                    "Basketball": []
+                                },
+                                {
+                                    "Cross country": []
+                                },
+                                {
+                                    "Fencing": []
+                                },
+                                {
+                                    "Field hockey": []
+                                },
+                                {
+                                    "Golf": []
+                                },
+                                {
+                                    "Lacrosse": []
+                                },
+                                {
+                                    "Rowing": []
+                                },
+                                {
+                                    "Soccer": []
+                                },
+                                {
+                                    "Swimming and diving": []
+                                },
+                                {
+                                    "Tennis": []
+                                },
+                                {
+                                    "Track and field": []
+                                },
+                                {
+                                    "Volleyball": []
+                                }
+                            ]
+                        }
+                    ]
+                },
+                {
+                    "Opinion": []
+                },
+                {
+                    "Recess": [
+                        {
+                            "Page Two": []
+                        },
+                        {
+                            "Arts": []
+                        },
+                        {
+                            "Music": []
+                        },
+                        {
+                            "Film and Literature": []
+                        }
+                    ]
+                },
+                {
+                    "Towerview": []
+                }
+            ]
     },
     {
         name: 'LAYOUT_GROUPS',
         description: 'Groups used for layouts',
-        defaultValue: {}
+        defaultValue: {
+                "Frontpage": {
+                    "namespace": [
+                        "Layouts",
+                        "Frontpage"
+                    ],
+                    "groups": [
+                        "Breaking",
+                        "Slideshow",
+                        "Left Headlines",
+                        "Right Headlines",
+                        "Opinion",
+                        "News",
+                        "Sports",
+                        "Recess",
+                        "Towerview"
+                    ]
+                },
+                "News": {
+                    "namespace": [
+                        "Layouts",
+                        "News"
+                    ],
+                    "groups": [
+                        "Featured",
+                        "Right Headlines",
+                        "Headlines",
+                        "Stories"
+                    ]
+                },
+                "Sports": {
+                    "namespace": [
+                        "Layouts",
+                        "Sports"
+                    ],
+                    "groups": [
+                        "Slideshow",
+                        "Stories"
+                    ]
+                },
+                "Opinion": {
+                    "namespace": [
+                        "Layouts",
+                        "Opinion"
+                    ],
+                    "groups": [
+                        "Featured",
+                        "Columnists",
+                        "Edit Board",
+                        "More Columnists"
+                    ]
+                },
+                "Recess": {
+                    "namespace": [
+                        "Layouts",
+                        "Recess"
+                    ],
+                    "groups": [
+                        "Featured",
+                        "Sandbox",
+                        "Interviews",
+                        "Reviews",
+                        "Stories"
+                    ]
+                },
+                "Towerview": {
+                    "namespace": [
+                        "Layouts",
+                        "Towerview"
+                    ],
+                    "groups": [
+                        "Featured",
+                        "Savvy",
+                        "Wisdom",
+                        "Editors Note",
+                        "Prefix"
+                    ]
+                }
+        }
     },
     {
         name: 'RSS_FEEDS',
         description: 'RSS feeds to check and store',
-        defaultValue: {}
+        defaultValue: [
+                {
+                    "title": "sportsblog",
+                    "url": "http://feeds.feedburner.com/chronicleblogs/sports"
+                },
+                {
+                    "title": "twitter-DukeChronicle",
+                    "url": "http://api.twitter.com/1/statuses/user_timeline.rss?screen_name=DukeChronicle"
+                },
+                {
+                    "title": "twitter-ChronicleRecess",
+                    "url": "http://api.twitter.com/1/statuses/user_timeline.rss?screen_name=ChronicleRecess"
+                },
+                {
+                    "title": "twitter-TowerviewMag",
+                    "url": "http://api.twitter.com/1/statuses/user_timeline.rss?screen_name=TowerviewMag"
+                },
+                {
+                    "title": "twitter-DukeBasketball",
+                    "url": "http://api.twitter.com/1/statuses/user_timeline.rss?screen_name=dukebasketball"
+                },
+                {
+                    "title": "twitter-ChronPhoto",
+                    "url": "http://api.twitter.com/1/statuses/user_timeline.rss?screen_name=ChronPhoto"
+                },
+                {
+                    "title": "twitter-ChronicleSports",
+                    "url": "http://api.twitter.com/1/statuses/user_timeline.rss?screen_name=chroniclesports"
+                },
+                {
+                    "title": "newsblog",
+                    "url": "http://feeds.feedburner.com/chronicleblogs/news"
+                },
+                {
+                    "title": "recessblog",
+                    "url": "http://feeds.feedburner.com/chronicleblogs/playground"
+                },
+                {
+                    "title": "blog-opinion",
+                    "url": "http://feeds.feedburner.com/chronicleblogs/backpages"
+                }
+            ]
     },
     {
         name: 'MAILCHIMP_API_KEY',
@@ -92,12 +397,12 @@ var configParams = [
     {
         name: 'MAILCHIMP_LIST_ID',
         description: 'MailChimp List ID',
-        defaultValue: ''
+        defaultValue: 'bc302eeb8d'
     },
     {
         name: 'MAILCHIMP_TEMPLATE_ID',
         description: 'MailChimp Template ID',
-        defaultValue: ''
+        defaultValue: '233513'
     }
 ];
 

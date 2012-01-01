5faee5488c6af07d85b219851daf487675d47f37
Jim Posen <jim.posen@gmail.com>
2012-01-01 12:20:33 -0500

    Put custom log code in new transport.

---
+++ thechronicle_modules/log/lib/console.js
@@ -0,0 +1,43 @@
+var util = require('util');
+var winston = require('winston');
+var Transport = winston.transports.Transport;
+var Console = winston.transports.Console;
+
+
+var CustomConsole = exports.CustomConsole = function (options) {
+  Console.call(this, options);
+
+  options = options || {};
+  
+  this.name      = 'custom_console';
+  this.msgStringify = options.msgStringify || function (obj) {
+      return obj;
+  };
+  this.metaStringify = options.metaStringify || function (obj) {
+      return obj;
+  };
+};
+
+//
+// Inherit from `winston.transports.Console`.
+//
+util.inherits(CustomConsole, Console);
+
+//
+// Expose the name of this Transport on the prototype
+//
+CustomConsole.prototype.name = 'custom_console';
+
+//
+// ### function log (level, msg, [meta], callback)
+// #### @level {string} Level at which to log the message.
+// #### @msg {string} Message to log
+// #### @meta {Object} **Optional** Additional metadata to attach
+// #### @callback {function} Continuation to respond to when complete.
+// Overridden logging method exposed to Winston. Metadata is optional.
+//
+CustomConsole.prototype.log = function (level, msg, meta, callback) {
+    if (typeof msg == "object") msg = this.msgStringify(msg);
+    if (typeof meta == "object") meta = this.metaStringify(meta);
+    Console.prototype.log(level, msg, meta, callback);
+};

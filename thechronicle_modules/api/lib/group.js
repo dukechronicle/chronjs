168c2e8ddcf14b7755df8085206990a5ec7b8dcc
Jim Posen <jim.posen@gmail.com>
2012-01-20 14:19:28 -0500

    Refactored group.docs.

--- thechronicle_modules/api/lib/group.js
+++ thechronicle_modules/api/lib/group.js
@@ -31,98 +31,23 @@ group.list = function(namespace, callback) {
     });
 };
 
-/*
-group.create = function(namespace, name, callback) {
-    //check if group exists
-    db.group.list({
-        key: namespace.concat(name)
-    }, function(err, res) {
-        if(res.length === 0) {
-            //doesn't exist
-               db.group.create(namespace, name, callback);
-        } else {
-            callback("group already exists", null);
-        }
-    });
-};
-
-group.add = function(docId, namespace, groupName, callback) {
-    db.group.add(docId, namespace, groupName, callback);
-};
-
-group.remove = function(docid, namespace, name, callback) {
-    nimble.map(name, function(item, cbck) {
-        cbck(null, function(acallback) {
-            db.group.remove(docid, item, acallback);
-        });
-    }, function(map_err, map_res) {
-        nimble.series(map_res, callback);
-    });
-};
-*/
 group.docs = function(namespace, group, callback) {
-    var start = Date.now();
     var redisKey = "group.docs:" + namespace.toString()
-    if (group)
-        rediskey += ":" + group.toString();
+    if (group) rediskey += ":" + group.toString();
 
     redis.client.get(redisKey, function(err, res) {
         if (res)
 	    callback(null, JSON.parse(res));
 	else
-	    db.group.docs(namespace, group, function(err, res) {
-                if (BENCHMARK) log.info("RECEIVED %d", Date.now() - start);
-
-                // if querying name space, map each group to it's own object
-                if (err)
+	    groupDocs(namespace, group, function(err, results) {
+		if (err)
 		    callback(err);
-                else if (!group) {
-                    var groupedResults = {};
-		    var currentArticle
-
-                    res.forEach(function (key, doc) {
-                        var groupName = key[1];
-                        var docType = key[3];
-
-                        if (docType === "article") {
-			    // retain reference to current article so images
-			    // that are processed next can easily access it
-			    currentArticle = doc;
-
-                            if (! (groupName in groupedResults))
-				groupedResults[groupName] = [];
-                            if (doc.urls)
-                                doc.url = "/article/" + doc.urls[doc.urls.length - 1];
-
-                            // remove body to save space
-                            delete doc.body;
-                            delete doc.renderedBody;
-
-                            groupedResults[groupName].push(doc);
-
-                            // TODO this should NEVER happen
-                            doc.images = {};
-                        }
-			else if (docType === "image") {
-                            var imageType = key[4];
-                            currentArticle.images[imageType] = doc;
-                        }
-                    });
-
-		    for (var groupName in groupedResults) {
-			groupedResults[groupName][0].cssClass = "first";
-                        groupedResults[groupName][groupedResults[groupName].length - 1].cssClass = "last";
-                    }
-
-                    redis.client.set(redisKey, JSON.stringify(groupedResults));
-                    redis.client.expire(redisKey, 2);
-                    callback(null, groupedResults);
-                }
 		else {
-                    // TODO modify this
-                    return callback(null, {});
-                }
-            });
+                    redis.client.set(redisKey, JSON.stringify(results));
+                    redis.client.expire(redisKey, 2);
+                    callback(null, results);
+		}
+	    });
     });
 };
 
@@ -136,4 +61,60 @@ group.remove = function(nameSpace, groupName, docId, callback) {
 
 group.getLayoutGroups = function() {
     return _.extend({}, config.get('LAYOUT_GROUPS'));
+};
+
+
+function groupDocs(namespace, group, callback) {
+    var start = Date.now();
+    db.group.docs(namespace, group, function(err, res) {
+        if (BENCHMARK) log.info("RECEIVED %d", Date.now() - start);
+
+        // if querying name space, map each group to it's own object
+        if (err)
+	    callback(err);
+        else if (!group) {
+            var groupedResults = {};
+	    var currentArticle
+
+            res.forEach(function (key, doc) {
+                var groupName = key[1];
+                var docType = key[3];
+
+                if (docType === "article") {
+		    // retain reference to current article so images
+		    // that are processed next can easily access it
+		    currentArticle = doc;
+		    
+                    if (! (groupName in groupedResults))
+			groupedResults[groupName] = [];
+                    if (doc.urls)
+                        doc.url = "/article/" + doc.urls[doc.urls.length - 1];
+		    
+                    // remove body to save space
+                    delete doc.body;
+                    delete doc.renderedBody;
+
+                    groupedResults[groupName].push(doc);
+
+                    // TODO this should NEVER happen
+                    doc.images = {};
+                }
+		else if (docType === "image") {
+                    var imageType = key[4];
+                    currentArticle.images[imageType] = doc;
+                }
+            });
+
+	    for (var groupName in groupedResults) {
+		groupedResults[groupName][0].cssClass = "first";
+                groupedResults[groupName][groupedResults[groupName].length - 1].cssClass = "last";
+            }
+	    
+	    callback(null, groupedResults);
+        }
+	else {
+            // TODO modify this
+            return callback(null, {});
+        }
+    });
 };

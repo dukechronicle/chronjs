c483ab76d2b8d2206568806ffe9d28c29ac57df8
Dean Chen <dean.chen@duke.edu>
2012-02-07 21:21:44 -0500

    Disable prevent duplicate on page.

--- public/js/admin/layout.js
+++ public/js/admin/layout.js
@@ -54,10 +54,10 @@ $(function() {
         containerElement.appendTo($(this));
 
         element.addClass("exists");
-        element.attr("draggable", false);
+        //element.attr("draggable", false);
         //$(this).append(element.get(0));
 
-        if (element.parent().data("groupname") && (element.parent().data("groupname") !== $(this).data("groupname"))) {
+        if (false && element.parent().data("groupname") && (element.parent().data("groupname") !== $(this).data("groupname"))) {
             removeFromPrevious(docId, element, $(this).data("groupname"), containerElement);
         } else {
             $.post("/api/group/add", {

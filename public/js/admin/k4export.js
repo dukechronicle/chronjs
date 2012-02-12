d12b81a15e5197313f8b45c5d8108042b5a208aa
Prithvi Prabahar <prithvi923@gmail.com>
2012-02-12 14:55:49 -0800

    Added dropdown in k4export.jade for image selesction

--- public/js/admin/k4export.js
+++ public/js/admin/k4export.js
@@ -1,10 +1,21 @@
 function editDocument(article) {
     $("#sub"+article.id).attr('disabled', 'disabled');
+    
     article.taxonomy = $("#tax"+article.id).val();
     article.authors = article.authors.toString();
+    
+    
+    
     $.post('/admin/edit', { doc: article }, function(data, status) {
 	if (status != 'success')
 	    alert("Taxonomy change for article '" + article.title + "' failed");
+	    
+
+
+
+
+
+	
 	$("#sub"+article.id).removeAttr('disabled');
     });
 }

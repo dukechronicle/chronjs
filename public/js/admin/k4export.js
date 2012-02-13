751df85f20794d77eebf5874083321ac7db8b77e
Prithvi Prabahar <prithvi923@gmail.com>
2012-02-12 22:05:32 -0800

    removed upload files

--- public/js/admin/k4export.js
+++ public/js/admin/k4export.js
@@ -4,18 +4,19 @@ function editDocument(article) {
     article.taxonomy = $("#tax"+article.id).val();
     article.authors = article.authors.toString();
     
-    
+    var imageString = $("#img"+article.id).val();
+    var imageData;
+    if (imageString.length > 0) {
+    	imageData = JSON.parse(imageString);
+    }
     
     $.post('/admin/edit', { doc: article }, function(data, status) {
 	if (status != 'success')
 	    alert("Taxonomy change for article '" + article.title + "' failed");
-	    
-
-
-
-
-
-	
-	$("#sub"+article.id).removeAttr('disabled');
+	if (imageData != null) {
+	  $.post('/admin/edit', {imageVersionId: imageData.imageVersions})
+	}
+	else
+		$("#sub"+article.id).removeAttr('disabled');
     });
 }

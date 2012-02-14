c22d7108bb2b7372f7a6c43d2ac572f40fccae59
Jim Posen <jim.posen@gmail.com>
2012-02-14 16:39:58 -0500

    Removed script tag from manage.jade.

---
+++ public/js/admin/manage.js
@@ -0,0 +1,44 @@
+define(['jquery'], function ($) {
+
+    $(function() {
+
+        $(".delete").click(function(event) {
+            event.preventDefault();
+
+            var anchor = $(event.target);
+            var title = anchor.attr('data-title');
+            var docId = anchor.attr('data-docId');
+            var docRev = anchor.attr('data-docRev');
+
+            $('<div id="dialog-confirm">Delete <strong>' + title + '?</strong></div>').appendTo('body');
+
+            $("#dialog-confirm").dialog({
+                dialogClass: 'delete-confirmation-dialog',
+                title: 'Delete Article',
+                resizable: false,
+                autoOpen: true,
+                height:140,
+                modal: true,
+                buttons: {
+                    "Delete": function() {
+
+                        $.ajax({
+                            type: 'DELETE',
+                            url: '/api/' + docId,
+                            data: 'rev=' + docRev,
+                            success: function() {
+                                $(this).remove();
+                                location.reload();
+                            }
+                        });
+                    },
+                    Cancel: function() {
+                        $(this).remove();
+                    }
+                }
+            });
+
+        });
+    });
+
+});

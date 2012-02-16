e293871e1d1eeb1c213279bb34ac6ead6c82819e
Jim Posen <jim.posen@gmail.com>
2012-02-16 15:14:13 -0500

    Implemented api/lib/survey.js. Needs testing.

--- thechronicle_modules/api/lib/survey.js
+++ thechronicle_modules/api/lib/survey.js
@@ -1,10 +1,43 @@
 var survey = exports;
 
+var db = require('../../db-abstract');
 
-survey.vote = function (id, answer, callback);
-survey.setSection = function (id, taxonomy, callback);
-survey.getSurvey = function (id, callback);
-survey.getBySection = function (taxonomy, limit, callback);
-survey.getByTitle = function (title, callback);
-survey.getByVotes = function (query, callback);
-survey.getByDate = function (query, callback);
+
+survey.vote = function (id, answer, callback) {
+    db.survey.getSurvey(id, function (err, doc) {
+        if (err)
+            callback(err);
+        else if (! (answer in doc.results))
+            callback(answer + ' is not an option for survey ' + id);
+        else {
+            doc.results[answer]++;
+            db.survey.edit(doc._id, doc._rev, doc, callback);
+        }
+    });
+};
+
+survey.setSection = function (id, taxonomy, callback) {
+    db.survey.edit(id, { taxonomy: taxonomy }, callback);
+};
+
+survey.getSurvey = function (id, callback) {
+    db.survey.getSurvey(id, callback);
+};
+
+survey.getBySection = function (taxonomy, limit, callback) {
+    db.survey.getBySection(taxonomy, { limit: limit }, callback);
+};
+
+survey.getByTitle = function (title, callback) {
+    db.survey.getByTitle(title, {}, callback);
+};
+
+survey.getByVotes = function (descending, query, callback) {
+    query = query || {};
+    if (descending) query.desending = true;
+    db.survey.getByVotes(query, callback);
+};
+
+survey.getByDate = function (query, callback) {
+    db.survey.getByDate(query, callback);
+};

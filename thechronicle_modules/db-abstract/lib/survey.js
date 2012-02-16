f42d0ce0690472072784e78203022ae00b4aebc8
Jim Posen <jim.posen@gmail.com>
2012-02-16 15:03:17 -0500

    Implemented db-abstract/lib/survey.js. Needs testing.

--- thechronicle_modules/db-abstract/lib/survey.js
+++ thechronicle_modules/db-abstract/lib/survey.js
@@ -1,10 +1,60 @@
 var survey = exports;
 
+var db = require('./db-abstract');
+var globalFunctions = require('../../global-functions');
 
-survey.add = function (title, answers, taxonomy, callback);
-survey.edit = function (id, fields, callback);
-survey.getSurvey = function (id, callback);
-survey.getBySection = function (taxonomy, query, callback);
-survey.getByTitle = function (title, query, callback);
-survey.getByVotes = function (query, callback);
-survey.getByDate = function (query, callback);
+var _ = require('underscore');
+
+
+survey.add = function (title, answers, taxonomy, callback) {
+    var survey = {
+        title: title,
+        taxonomy: taxonomy,
+        created: globalFunctions.getTimestamp(),
+        type: 'survey',
+        results: _.reduce(answers,
+                          function (memo, answer) { memo[answer] = 0 },
+                          {});
+    };        
+    db.save(survey, callback);
+};
+
+survey.edit = function (id, fields, callback) {
+    db.get(id, function (err, doc) {
+        if (err) callback(err);
+        else db.merge(id, doc._rev, fields, callback);
+    });
+};
+
+survey.getSurvey = function (id, callback) {
+    db.get(id, callback);
+};
+
+survey.getBySection = function (taxonomy, query, callback) {
+    query = query || {};
+    if (query.descending) {
+        query.startkey = [taxonomy];
+        query.endkey = [taxonomy, {}];
+    }
+    else {
+        query.startkey = [taxonomy, {}];
+        query.endkey = [taxonomy];
+    }
+    db.view('survey/taxonomy', query, callback);
+};
+
+survey.getByTitle = function (title, query, callback) {
+    query = query || {};
+    query.key = title;
+    db.view('survey/title', query, callback);
+};
+
+survey.getByVotes = function (query, callback) {
+    query = query || {};
+    db.view('survey/votes', query, callback);
+};
+
+survey.getByDate = function (query, callback) {
+    query = query || {};
+    db.view('survey/date', query, callback);
+};

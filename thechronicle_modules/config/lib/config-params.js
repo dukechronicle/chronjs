fcd098f7502b8196d062c8e5c0bb504157724c17
Joe Levy <jal50@duke.edu>
2012-02-19 04:36:05 -0500

    start support for schemas

--- thechronicle_modules/config/lib/config-params.js
+++ thechronicle_modules/config/lib/config-params.js
@@ -1,73 +1,118 @@
+// for JSON schema design, see http://tools.ietf.org/html/draft-zyp-json-schema-03
+// after designing a schema, you can test it here: http://james.newtonking.com/projects/json/schema.aspx
+
 var configParams = [
     {
         name: 'ADMIN_USERNAME',
         description: 'New administrator username to login with from now on',
-        defaultValue: 'admin'
+        defaultValue: 'admin',
+        schema: {
+          type: "string",
+        }
     },
     {
         name: 'ADMIN_PASSWORD',
         description: 'New administrator password to login with from now on',
-        defaultValue: 'chronicle'
+        defaultValue: 'chronicle',
+        schema: {
+          type: "string",
+        }
     },
     {
         name: 'DISQUS_SHORTNAME',
         description: 'Disqus Shortname',
-        defaultValue: 'chronicletest'
+        defaultValue: 'chronicletest',
+        schema: {
+          type: "string",
+        }
     },
     {
         name: 'COUCHDB_URL',
         description: 'CouchDB Server URL',
-        defaultValue: 'http://chrondev:pikachu@chrondev.iriscouch.com'
+        defaultValue: 'http://chrondev:pikachu@chrondev.iriscouch.com',
+        schema: {
+          type: "string",
+        }
     },
     {
         name: 'COUCHDB_DATABASE',
         description: 'CouchDB Database Name',
-        defaultValue: 'dev'
+        defaultValue: 'dev',
+        schema: {
+          type: "string",
+        }
     },
     {
         name: 'S3_BUCKET',
         description: 'Amazon S3 Bucket Name',
-        defaultValue: 'chron_dev'
+        defaultValue: 'chron_dev',
+        schema: {
+          type: "string",
+        }
     },
     {
         name: 'S3_KEY',
         description: 'Amazon S3 Key',
-        defaultValue: ''
+        defaultValue: '',
+        schema: {
+          type : "string",
+        }
     },
     {
         name: 'S3_SECRET',
         description: 'Amazon S3 Secret Key',
-        defaultValue: ''
+        defaultValue: '',
+        schema: {
+          type: "string",
+        }
     },
     {
         name: 'CLOUDFRONT_DISTRIBUTION',
         description: 'Amazon CloudFront Domain Name',
-        defaultValue: 'http://d164gy67scumxg.cloudfront.net'
+        defaultValue: 'http://d164gy67scumxg.cloudfront.net',
+        schema: {
+          type: "string",
+        }
     },
     {
         name: 'REDIS_URL',
         description: 'Redis Server URL',
-        defaultValue: 'redis://jodoglevy:2b258cbdcbbce003452a0ff4279d8701@barreleye.redistogo.com:9148/'
+        defaultValue: 'redis://jodoglevy:2b258cbdcbbce003452a0ff4279d8701@barreleye.redistogo.com:9148/',
+        schema: {
+          type: "string",
+        }
     },
     {
         name: 'SOLR_HOST',
         description: 'WebSolr Server URL',
-        defaultValue: 'index.websolr.com'
+        defaultValue: 'index.websolr.com',
+        schema: {
+          type: "string",
+        }
     },
     {
         name: 'SOLR_PORT',
         description: 'WebSolr Server Port',
-        defaultValue: '80'
+        defaultValue: '80',
+        schema: {
+          type: "string",
+        }
     },
     {
         name: 'SOLR_CORE',
         description: 'WebSolr Core',
-        defaultValue: '/3f534ff3ff0'
+        defaultValue: '/3f534ff3ff0',
+        schema: {
+          type: "string",
+        }
     },
     {
         name: 'SOLR_PATH',
         description: 'WebSolr Server Path',
-        defaultValue: '/solr'
+        defaultValue: '/solr',
+        schema: {
+          type: "string",
+        }
     },
     {
         name: 'TAXONOMY_MAIN_SECTIONS',
@@ -78,7 +123,11 @@ var configParams = [
             "Opinion",
             "Recess",
             "Towerview"
-        ]
+        ],
+        schema: {
+            type: "array",
+            items: {type: "string"}
+        }
     },
     {
         name: 'TAXONOMY',
@@ -282,7 +331,17 @@ var configParams = [
 	        {
 		        "Towerview": []
 	        }
-        ]
+        ],
+        schema: {
+            type: "array"
+            id: "taxonomyLevel",
+            items: {
+                type: "object",
+                additionalProperties: {
+                    "$ref": "taxonomyLevel"
+                }
+            }
+        }
     },
     {
         name: 'LAYOUT_GROUPS',
@@ -376,6 +435,22 @@ var configParams = [
 			        "Towerview"
 		        ]
 	        }
+        },
+        schema: {
+            type: "object",
+            additionalProperties: {
+                type: "object",
+                properties: {
+                    namespace: {
+                        type:"array",
+                        items: {type: "string"}
+                    },
+                    groups: {
+                        type: "array",
+                        items: {type: "string"}
+                    }
+                }
+            }
         }
     },
     {
@@ -422,22 +497,41 @@ var configParams = [
                     "title": "blog-opinion",
                     "url": "http://feeds.feedburner.com/chronicleblogs/backpages"
                 }
-            ]
+            ],
+        schema: {
+          type: "array",
+          items: {
+            type: "object",
+            properties: {
+                title: {type: "string"},
+                url: {type: "string"}
+            }
+          }          
+       }
     },
     {
         name: 'MAILCHIMP_API_KEY',
         description: 'MailChimp API key',
-        defaultValue: ''
+        defaultValue: '',
+        schema: {
+          type: "string",
+        }
     },
     {
         name: 'MAILCHIMP_LIST_ID',
         description: 'MailChimp List ID',
-        defaultValue: '21f5467afc'
+        defaultValue: '21f5467afc',
+        schema: {
+          type: "string",
+        }
     },
     {
         name: 'MAILCHIMP_TEMPLATE_ID',
         description: 'MailChimp Template ID',
-        defaultValue: '429'
+        defaultValue: '429',
+        schema: {
+          type: "string",
+        }
     },
     {
         name: 'MULTIMEDIA_HTML',
@@ -460,7 +554,14 @@ var configParams = [
                 '<object classid="clsid:D27CDB6E-AE6D-11cf-96B8-444553540000" width="234" height="210"><param name="movie" value="http://www.photoshelter.com/swf/CSlideShow.swf?feedSRC=http%3A//dukechronicle.photoshelter.com/gallery/10-6-11-Shakori-Hills-Grassroots-Festival/G00009z37ixb7pJc%3Ffeed%3Djson"></param><param name="wmode" value="opaque"></param><param name="allowScriptAccess" value="always"></param><param name="allowFullScreen" value="true"></param><param name="bgColor" value="#AAAAAA"></param><param name="flashvars" value="target=_self&f_l=f&f_fscr=t&f_tb=t&f_bb=f&f_bbl=f&f_fss=f&f_2up=t&f_crp=t&f_wm=t&f_s2f=t&f_emb=t&f_cap=t&f_sln=t&imgT=iptch&cred=iptc&trans=xfade&f_link=t&f_smooth=t&f_mtrx=t&tbs=5000&f_ap=t&f_up=f&btype=old&bcolor=%23CCCCCC"></param><!--[if !IE]><!--><object type="application/x-shockwave-flash" data="http://www.photoshelter.com/swf/CSlideShow.swf?feedSRC=http%3A//dukechronicle.photoshelter.com/gallery/10-6-11-Shakori-Hills-Grassroots-Festival/G00009z37ixb7pJc%3Ffeed%3Djson" width="234" height="210" ><param name="wmode" value="opaque"></param><param name="allowScriptAccess" value="always"></param><param name="allowFullScreen" value="true"></param><param name="bgColor" value="#AAAAAA"></param><param name="flashvars" value="target=_self&f_l=f&f_fscr=t&f_tb=t&f_bb=f&f_bbl=f&f_fss=f&f_2up=t&f_crp=t&f_wm=t&f_s2f=t&f_emb=t&f_cap=t&f_sln=t&imgT=iptch&cred=iptc&trans=xfade&f_link=t&f_smooth=t&f_mtrx=t&tbs=5000&f_ap=t&f_up=f&btype=old&bcolor=%23CCCCCC"></param><!--<![endif]--><a href="http://dukechronicle.photoshelter.com/gallery/10-6-11-Shakori-Hills-Grassroots-Festival/G00009z37ixb7pJc"><img src="http://www.photoshelter.com/gal-kimg-get/G00009z37ixb7pJc/s/234/210" alt="" /></a><!--[if !IE]><!--></object><!--<![endif]--></object>',
                 '<object classid="clsid:D27CDB6E-AE6D-11cf-96B8-444553540000" width="234" height="210"><param name="movie" value="http://www.photoshelter.com/swf/CSlideShow.swf?feedSRC=http%3A//dukechronicle.photoshelter.com/gallery/10-6-11-Dark-Dark-Dark/G0000qK2lm8.Y61o%3Ffeed%3Djson"></param><param name="wmode" value="opaque"></param><param name="allowScriptAccess" value="always"></param><param name="allowFullScreen" value="true"></param><param name="bgColor" value="#AAAAAA"></param><param name="flashvars" value="target=_self&f_l=f&f_fscr=t&f_tb=t&f_bb=f&f_bbl=f&f_fss=f&f_2up=t&f_crp=t&f_wm=t&f_s2f=t&f_emb=t&f_cap=t&f_sln=t&imgT=iptch&cred=iptc&trans=xfade&f_link=t&f_smooth=t&f_mtrx=t&tbs=5000&f_ap=t&f_up=f&btype=old&bcolor=%23CCCCCC"></param><!--[if !IE]><!--><object type="application/x-shockwave-flash" data="http://www.photoshelter.com/swf/CSlideShow.swf?feedSRC=http%3A//dukechronicle.photoshelter.com/gallery/10-6-11-Dark-Dark-Dark/G0000qK2lm8.Y61o%3Ffeed%3Djson" width="234" height="210" ><param name="wmode" value="opaque"></param><param name="allowScriptAccess" value="always"></param><param name="allowFullScreen" value="true"></param><param name="bgColor" value="#AAAAAA"></param><param name="flashvars" value="target=_self&f_l=f&f_fscr=t&f_tb=t&f_bb=f&f_bbl=f&f_fss=f&f_2up=t&f_crp=t&f_wm=t&f_s2f=t&f_emb=t&f_cap=t&f_sln=t&imgT=iptch&cred=iptc&trans=xfade&f_link=t&f_smooth=t&f_mtrx=t&tbs=5000&f_ap=t&f_up=f&btype=old&bcolor=%23CCCCCC"></param><!--<![endif]--><a href="http://dukechronicle.photoshelter.com/gallery/10-6-11-Dark-Dark-Dark/G0000qK2lm8.Y61o"><img src="http://www.photoshelter.com/gal-kimg-get/G0000qK2lm8.Y61o/s/234/210" alt="" /></a><!--[if !IE]><!--></object><!--<![endif]--></object>'
             ]
-        }
+        },
+        schema: {
+          type: "object",
+          additionalProperties: {
+            type: "array",
+            items: {type : "string"}
+          }
+       }
     }
 ];
 

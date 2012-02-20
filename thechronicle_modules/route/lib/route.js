ad7a9cda3c07b1c1c11544524efa7d9857d0533b
Jim Posen <jim.posen@gmail.com>
2012-02-20 03:51:01 -0500

    Fixed external api taxonomy call.

--- thechronicle_modules/route/lib/route.js
+++ thechronicle_modules/route/lib/route.js
@@ -20,7 +20,7 @@ exports.init = function (app) {
 
     app.namespace('/api', function () {
         app.get('/All', siteApi.listAll);
-        app.get('/:groupname', siteApi.section);
+        app.get('/:section', siteApi.section);
         app.get('/article/:url', siteApi.article);
         app.get('/search/:query', siteApi.search);
         app.get('/staff/:query', siteApi.staff);

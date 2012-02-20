1f6eca807f67a09b15f5a9bd5b1865da3b1c7dd0
Joe Levy <jal50@duke.edu>
2012-02-19 19:49:59 -0500

    tiny mobile fix

--- public/js/mobile-redirect.js
+++ public/js/mobile-redirect.js
@@ -5,7 +5,7 @@ var userAgent = navigator.userAgent || "";
                                                                                            
 for(var i in MOBILE_BROWSER_USER_AGENTS) {                                            
 	if(userAgent.indexOf(MOBILE_BROWSER_USER_AGENTS[i]) != -1) {                         
-        window.location='/m/' + window.location.pathname;                                                                                    
+        window.location='/m' + window.location.pathname;                                                                                    
     }                                                                  
 }                                                                                        
                                                                                              

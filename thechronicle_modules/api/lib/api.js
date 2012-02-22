61cd886b48ff77eafa576772dc468884b0e5103f
Jim Posen <jim.posen@gmail.com>
2012-02-21 21:59:04 -0500

    Refactored api.editDoc and api.addDoc.

--- thechronicle_modules/api/lib/api.js
+++ thechronicle_modules/api/lib/api.js
@@ -1,11 +1,15 @@
 var api = exports;
 
-var nimble = require("nimble");
-var async = require("async");
-var db = require("../../db-abstract");
-var log = require('../../log');
 var config = require("../../config");
+var db = require("../../db-abstract");
 var globalFunctions = require("../../global-functions");
+var log = require("../../log");
+var redis = require('../../redisclient');
+
+var async = require("async");
+var md = require('node-markdown').Markdown;
+var nimble = require("nimble");
+var sprintf = require('sprintf').sprintf;
 var _ = require("underscore");
 
 api.group = require("./group");
@@ -20,10 +24,16 @@ api.database = require("./database");
 api.s3 = require('./s3');
 api.site = require('./site');
 
-var redis = require('../../redisclient');
 
 var MAX_URL_LENGTH = 50;
 var RESULTS_PER_PAGE = 25;
+
+var VIDEO_PLAYERS = {
+    "youtube": "<iframe width=\"560\" height=\"345\" src=\"http://www.youtube.com/embed/%s\" frameborder=\"0\" allowfullscreen></iframe>",
+    "vimeo": "<iframe src=\"http://player.vimeo.com/video/%s?title=0&amp;byline=0&amp;portrait=0\" width=\"400\" height=\"225\" frameborder=\"0\"></iframe>"
+};
+var VIDEO_REGEX_FORMAT = "(\{%s:)([^}]+)(\})";
+
 
 function getAvailableUrl(url, n, callback) {
     var new_url = url;
@@ -45,10 +55,7 @@ function getAvailableUrl(url, n, callback) {
 
 //from http://snipt.net/jpartogi/slugify-javascript/
 function _URLify(s, maxChars) {
-
-    if(maxChars === undefined) {
-        maxChars = 100;
-    }
+    maxChars = maxChars || MAX_URL_LENGTH;
 
     var removelist = ["a", "an", "as", "at", "before", "but", "by", "for", "from",
                   "is", "in", "into", "like", "of", "off", "on", "onto", "per",
@@ -63,6 +70,17 @@ function _URLify(s, maxChars) {
     s = s.replace(/[-\s]+/g, "-");   // convert spaces to hyphens
     s = s.toLowerCase();             // convert to lowercase
     return s.substring(0, maxChars);// trim to first num_chars chars
+}
+
+function renderBody(body, callback) {
+    for(var name in VIDEO_PLAYERS) {
+        var pattern = new RegExp(sprintf(REGEX_FORMAT, name), 'g');
+        body = body.replace(pattern, function(match) {
+            return sprintf(VIDEO_PLAYERS[name], RegExp.$2);
+        });
+    }
+
+    callback(null, md(body));
 }
 
 api.init = function(callback) {
@@ -103,44 +121,46 @@ api.getArticles= function(parent_node, count, callback) {
     callback);
 };
 
+api.addDoc = function(fields, callback) {
+    if (fields.type === 'article') {
+        getAvailableUrl(_URLify(fields.title), 0, function(err, url) {
+            if (err) return callback(err);
+
+            var unix_timestamp = globalFunctions.unixTimestamp();
+            fields.created = fields.created || unix_timestamp;
+            fields.updated = fields.created || unix_timestamp;
+            fields.urls = [url];
+            fields.indexedBySolr = api.search.getIndexVersion();
+                
+            // strip all html tags from the teaser
+            fields.teaser = fields.teaser.replace(/<(.|\n)*?>/g,"");
+
+            db.save(fields, function(err, res) {
+                if (err) return callback(err);
+                api.search.indexArticle(res.id, fields.title, fields.body,
+                                        fields.taxonomy, fields.authors,
+                                        fields.created, callback);
+            });
+        });
+    }
+    else callback("Unknown document type");
+};
+
 api.editDoc = function(docid, fields, callback) {
-    var indexFunc = function(err, res, url) {
- 	     if(err) return callback(err, res, url);	
-       
-         // only reindex the article if they edited the search fields            
-         if(fields.title && fields.body) {
-             api.search.indexArticle(docid, fields.title, fields.body, fields.taxonomy, fields.authors, fields.created, function(err2) {
-                 callback(err2, res, url);
-             });
-         }
-         else callback(err, res, url);	
-    };
-
-    api.docsById(docid, function(geterr, res) {
-        if(geterr) return callback(geterr, null, null);
-
-
-        if(res.created) fields.created = res.created;
-
-        if(fields.title && (_URLify(fields.title, MAX_URL_LENGTH) !== _URLify(res.title, MAX_URL_LENGTH))) {
-            getAvailableUrl(_URLify(fields.title, MAX_URL_LENGTH), 0, function(err, url) {
-                if(err) return callback(err, null, null);
-               
+    api.docsById(docid, function(err, res) {
+        if (err) callback(err);
+        else if (fields.title && (_URLify(fields.title) !=  _URLify(res.title))){
+            getAvailableUrl(_URLify(fields.title), 0, function(err, url) {
+                if (err) return callback(err);
+                
                 fields.updated = globalFunctions.unixTimestamp();
                 fields.urls = res.urls;
                 fields.urls.push(url);
-                db.merge(docid, fields, function(db_err, db_res) {
-                    indexFunc(db_err, db_res, url);
-                });
+
+                saveEditedDoc(docid, fields, url, callback);
             });
         }
-        else {
-            db.merge(docid, fields, function(db_err, db_res) {
-                if (db_err) return callback(db_err);
-                
-                indexFunc(db_err, db_res, res.urls[res.urls.length - 1]);
-            });
-        }    
+        else saveEditedDoc(docid, fields, _.last(res.urls), callback);
     });
 };
 
@@ -163,37 +183,6 @@ api.docsByAuthor = function(author, callback) {
         if (err) callback(err);
         else callback(null, _.map(docs, function(doc){return doc.value}));
     });
-};
-
-api.addDoc = function(fields, callback) {
-    if (fields.type === 'article') {
-        getAvailableUrl(_URLify(fields.title, MAX_URL_LENGTH), 0, function(err, url) {
-            if(err){
-                return callback(err, null, null);
-            }
-            else {
-                var unix_timestamp = globalFunctions.unixTimestamp();
-                fields.created = fields.created || unix_timestamp;
-                fields.updated = fields.created || unix_timestamp;
-                fields.urls = [url];
-                fields.indexedBySolr = api.search.getIndexVersion();
-                
-                // strip all html tags from the teaser
-                fields.teaser = fields.teaser.replace(/<(.|\n)*?>/g,"");
-
-                db.save(fields, function(db_err, res) {
-
-                    if(db_err) return callback(db_err);
-
-                    api.search.indexArticle(res.id, fields.title, fields.body, fields.taxonomy, fields.authors, fields.created, function(err) {
-                        callback(err,url,res.id);
-                    });
-                });
-            }
-        });
-    } else {
-        return callback("unknown doc type", null);
-    }
 };
 
 api.addNode = function(parent_path, name, callback) {
@@ -356,3 +345,18 @@ api.getDatabaseHost = function() {
 api.getDatabasePort = function() {
     return db.getDatabasePort();
 };
+
+function saveEditedDoc(docid, doc, url, callback) {
+    db.merge(docid, doc, function(err, res) {
+ 	if (err) callback(err);
+        // only reindex the article if they edited the search fields
+        else if (doc.title && doc.body) {
+            api.search.indexArticle(docid, res.title, res.body, res.taxonomy,
+                                    res.authors, res.created, function (err) {
+                                        if (err) callback(err);
+                                        else callback(null, res, url);
+                                    });
+        }
+        else callback(null, res, url);
+    });
+}

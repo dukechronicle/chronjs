b6d44085b0726a295f461012f622d1c610b5caf1
Jim Posen <jim.posen@gmail.com>
2012-03-02 10:30:44 -0500

    Made tagline not required for columnists.

--- thechronicle_modules/config/lib/config-params.js
+++ thechronicle_modules/config/lib/config-params.js
@@ -649,7 +649,7 @@ var configParams = [
                     name: {type: "string", required: true},
                     user: {type: "string", required: false},
                     year: {type: "string", required: true},
-                    tagline: {type: "string", required: true},
+                    tagline: {type: "string", required: false},
                     day: {type: "string", required: true},
                     headshot: {type: "string", required: true}
                 }

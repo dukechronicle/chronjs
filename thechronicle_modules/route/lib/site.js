d40b6021795100e97d652124a08a343c73075f43
Jim Posen <jim.posen@gmail.com>
2012-03-12 13:42:57 -0400

    Put disqus js loading in site.js instead of article.jade.

--- thechronicle_modules/route/lib/site.js
+++ thechronicle_modules/route/lib/site.js
@@ -184,7 +184,8 @@ site.article = function (req, res, next) {
                 disqusShortname: config.get('DISQUS_SHORTNAME')
             },
             filename:'views/article',
-            css:asereje.css(['container/style', 'article'])
+            css:asereje.css(['container/style', 'article']),
+            js:['disqus?v=4']
         });
     });
 };

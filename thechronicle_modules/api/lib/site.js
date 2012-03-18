d1628af3045f9efb8f414bc41a3290f5a250a576
Jim Posen <jim.posen@gmail.com>
2012-03-18 17:58:20 -0400

    Removed cache busting measure used for debugging.

--- thechronicle_modules/api/lib/site.js
+++ thechronicle_modules/api/lib/site.js
@@ -487,7 +487,7 @@ site.getArticleContent = function(url, callback) {
     var redisKey = "article:" + url;
 
     redis.client.get(redisKey, function(err, res) {
-        if (res && false) {
+        if (res) {
             var data = JSON.parse(res);
             callback(null, data[0], data[1], data[2]);
         } else {

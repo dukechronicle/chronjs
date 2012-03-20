b68162b456ed74ff8b03a9ad268e967066ed0db1
Jim Posen <jim.posen@gmail.com>
2012-03-20 00:00:12 -0400

    All admin Javascript loaded together. The delete actions interfere.

---
+++ public/js/admin/main.js
@@ -0,0 +1,13 @@
+// All Javascript files used by the admin pages
+var SCRIPTS = ['admin/crop','admin/deleteArticle','admin/html5upload',
+               'admin/imgdelete','admin/json-to-form','admin/k4export',
+               'admin/layout','admin/manage','admin/newsletter','admin/nicedate']
+
+require.config({
+    baseUrl: '/js',
+    paths: {
+        'order': '/js/libs/order'
+    }
+});
+
+require(SCRIPTS);

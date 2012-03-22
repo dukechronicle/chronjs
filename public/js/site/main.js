6dff4b7cd4593308630491ade7d9776ea3efec6d
Jim Posen <jim.posen@gmail.com>
2012-03-22 15:26:00 -0400

    Moved align calls into align.js.

--- public/js/site/main.js
+++ public/js/site/main.js
@@ -5,12 +5,8 @@ require.config({
 require(['site/align','site/article','site/opinion','site/scrollLoad',
          'site/slideshow/frontpage-slideshow','site/slideshow/slideshow'],
         function (align) {
-            loadAfterTypekit(function() {
-                align.pageAlign();
-                if (page() === 'front') align.frontpageAlign();
-                align.verticalAlign();
-            });
-});
+            loadAfterTypekit(align);
+        });
 
 function loadAfterTypekit(callback) {
     if ($('html').hasClass("wf-active") || $('html').hasClass("wf-inactive")) {
@@ -18,14 +14,5 @@ function loadAfterTypekit(callback) {
     } else {
         var retry = function() {loadAfterTypekit(callback)};
         setTimeout(retry, 300)
-    }
-}
-
-function page() {
-    var path = document.location.href.split("/")[3];
-    if (! path) {
-        return "front";
-    } else {
-        return path.split("#")[0];
     }
 }

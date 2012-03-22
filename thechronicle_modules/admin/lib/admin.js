8ea51c8816998aedd73520e00d1ba9091064666f
Jim Posen <jim.posen@gmail.com>
2012-03-21 21:50:59 -0400

    Got rid of all 'js' variables in res.render option objects.

--- thechronicle_modules/admin/lib/admin.js
+++ thechronicle_modules/admin/lib/admin.js
@@ -25,7 +25,6 @@ admin.newsletter = function (req, res, next) {
         if (err) next(err);
         else {
             res.render('admin/newsletter', {
-                js: ['admin/newsletter?v=3'],
                 locals: {campaignID: campaignID}
             });
         }
@@ -86,7 +85,6 @@ admin.manage = function (req, res, next) {
     api.docsByDate(null, query, function (err, docs) {
         if (err) next(err);
         else res.render('admin/manage', {
-            js: ['admin/manage?v=2'],
             locals:{
                 docs:docs,
                 hasPrevious:(req.query.beforeID != null),
@@ -169,7 +167,6 @@ admin.k4exportData = function (req, res, next) {
     function(err, results) {
         res.render('admin/k4export', {
 	        css: ['css/msdropdown'],
-            js: ['admin/k4export?v=7'],
             locals:{
                 failed: results.k4.failed,
                 succeeded: results.k4.success,

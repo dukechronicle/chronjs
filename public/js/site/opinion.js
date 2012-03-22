5125c5c282629894f0ad30d2beebcfa89909f7cd
Jim Posen <jim.posen@gmail.com>
2012-03-21 21:04:26 -0400

    Opinion page Javascript happens on DOM ready.

--- public/js/site/opinion.js
+++ public/js/site/opinion.js
@@ -1,22 +1,27 @@
 define(['jquery'], function($) {
-    $('#columnists').show();
-    var selected = $([]);
-    var selectedNav = $([]);
-    $('#columnist-list .content-container').on("click", "a", function(e) {
-        e.preventDefault();
-        selectedNav.removeClass('selected');
-        selected.hide();
-        selectedNav = $(this);
-        show(selectedNav.attr('data-index'));
+
+    $(function () {
+        $('#columnists').show();
+        var selected = $([]);
+        var selectedNav = $([]);
+        $('#columnist-list .content-container').on("click", "a", function(e) {
+            e.preventDefault();
+            selectedNav.removeClass('selected');
+            selected.hide();
+            selectedNav = $(this);
+            show(selectedNav.attr('data-index'));
+        });
+        
+        // randomly select a columnist
+        var columnistAnchors = $('#columnist-list h2 a');
+        var randomIndex = Math.floor(Math.random()*columnistAnchors.size());
+        columnistAnchors.eq(randomIndex).click();
     });
 
-    // randomly select a columnist
-    var columnistAnchors = $('#columnist-list h2 a');
-    var randomIndex = Math.floor(Math.random()*columnistAnchors.size());
-    columnistAnchors.eq(randomIndex).click();
     function show(index) {
         selected = $("#columnist-story-" + index);
         selectedNav.addClass('selected');
         selected.show();
     }
+
 });

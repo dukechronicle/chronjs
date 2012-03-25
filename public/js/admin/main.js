c5296323c05581dab7f99b14c1eba8f8162f3fe1
Jim Posen <jim.posen@gmail.com>
2012-03-25 17:21:42 -0400

    Put CHRONICLE.onload in admin pages.

--- public/js/admin/main.js
+++ public/js/admin/main.js
@@ -4,4 +4,10 @@ require.config({
 
 require(['admin/crop','admin/deleteArticle','admin/html5upload',
          'admin/imgdelete','admin/json-to-form','admin/k4export',
-         'admin/layout','admin/newsletter','admin/nicedate']);
+         'admin/layout','admin/newsletter','admin/nicedate'], function () {
+             $(function () {
+                 if (typeof CHRONICLE == "object" && CHRONICLE.onload)
+                     for (var i in CHRONICLE.onload)
+                         CHRONICLE.onload[i]();
+             });
+         });

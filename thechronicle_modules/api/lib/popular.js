bbe56ca470b382edae7e3433252efb9f775cf5da
Alex Galonsky <alex@galonsky.com>
2012-03-25 17:56:23 -0400

    Removing articles from popular database when over a week old.

--- thechronicle_modules/api/lib/popular.js
+++ thechronicle_modules/api/lib/popular.js
@@ -2,25 +2,51 @@ var _ = require("underscore");
 var log = require('../../log');
 var redis = require('../../redisclient');
 var config = require('../../config');
+var globalFunctions = require("../../global-functions");
 
 var popular = exports;
+var POPULAR_EXPIRATION_SECS = 60*60*24*7; //one week
 
 popular.registerArticleView = function(doc, callback) {
+	var unix_timestamp = globalFunctions.unixTimestamp();
     if(doc.taxonomy) {
-		var length = doc.taxonomy.length;
+    	var length = doc.taxonomy.length;
 		var taxToSend = _.clone(doc.taxonomy);
-		var multi = redis.client.multi();
-		for(var i = length; i >= 0; i--) {
-			taxToSend.splice(i, 1);
-			multi.zincrby(_articleViewsKey(taxToSend), 1, _.last(doc.urls) + "||" + doc.title);
-		}
-		multi.exec(function(err, res) {
-			if(err) {
-				log.warning("Failed to register article view: " + _.last(doc.urls));
-				log.warning(err);
+
+		if(unix_timestamp - doc.updated > POPULAR_EXPIRATION_SECS) {
+			//remove from redis
+			
+			var multi = redis.client.multi();
+			for(var i = length; i >= 0; i--) {
+				taxToSend.splice(i, 1);
+				multi.zrem(_articleViewsKey(taxToSend), _.last(doc.urls) + "||" + doc.title);
 			}
-			callback(err, res);
-		});
+			log.info('Removing article from popular: ' + _.last(doc.urls));
+			multi.exec(function(err, res) {
+				if(err) {
+					log.warning("Failed to delete popular article: " + _.last(doc.urls));
+					log.warning("Redis probably throwing error because article has already been deleted.");
+					log.warning(err);
+				}
+				callback(err, res);
+			});
+
+		} else {
+			var multi = redis.client.multi();
+			for(var i = length; i >= 0; i--) {
+				taxToSend.splice(i, 1);
+				multi.zincrby(_articleViewsKey(taxToSend), 1, _.last(doc.urls) + "||" + doc.title);
+			}
+			multi.exec(function(err, res) {
+				if(err) {
+					log.warning("Failed to register article view: " + _.last(doc.urls));
+					log.warning(err);
+				}
+				log.info('Incrementing article view: ' + _.last(doc.urls));
+				callback(err, res);
+			});
+		}
+		
 	}
 }
 

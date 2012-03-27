e8f0196638e19a85a883f7e8656c49c5aeda8555
Jim Posen <jim.posen@gmail.com>
2012-03-27 18:09:33 -0400

    Shows poll results if user already voted.

--- public/js/site/poll.js
+++ public/js/site/poll.js
@@ -1,10 +1,11 @@
+var displayPoll;
+
 define(['jquery'], function ($) {
 
-    var voted = false;
-    var total = 1;
+    var total = 0;
 
-    $(function () {
-        
+    displayPoll = function (voted) {
+
         $(".poll .choice").each(function () {
             total += parseInt($(this).attr("votes"));
         });
@@ -13,32 +14,39 @@ define(['jquery'], function ($) {
             e.preventDefault();
 
             var $choice = $(this).parent();
-
+            
             if (!voted) {
                 voted = true;
 
                 var id = $(".poll > .container").attr("id");
                 var answer = $choice.attr("id");
                 $choice.attr("votes", function (index, votes) {
+                    total++;
                     return parseInt(votes) + 1;
                 });
 
                 $.post('/api/poll/vote', { id: id, answer: answer });
-
-                $(".choice").each(function () {
-                    var percent = parseInt($(this).attr("votes")) / total;
-                    $(this).append("<p>"+Math.round(percent * 100) + "%</p>");
-                    $(this).append("<div>");
-                    $(this).children(":last")
-                        .addClass("bar")
-                        .css("width", 0)
-                        .animate({
-                            width: $(this).width() * percent
-                        }, 1000);
-                });
+                
+                showResults();
             }
         });
-            
-    });
+
+        if (voted) showResults();
+
+    }
+
+    function showResults() {
+        $(".poll .choice").each(function () {
+            var percent = parseInt($(this).attr("votes")) / total;
+            $(this).append("<p>"+Math.round(percent * 100) + "%</p>");
+            $(this).append("<div>");
+            $(this).children(":last")
+                .addClass("bar")
+                .css("width", 0)
+                .animate({
+                    width: $(this).width() * percent
+                }, 1000);
+        });
+    }
 
 });

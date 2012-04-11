aee2d7f8bef7e24ce7b6e1b21963d2910c4ed204
Glenn Rivkees <glenn.rivkees@gmail.com>
2012-04-11 14:04:29 -0400

    Revert "Author pages image implemented"
    This reverts commit a3070c3f23308911708474d1bd9d864fee0561c0.

--- thechronicle_modules/db-abstract/lib/authors.js
+++ thechronicle_modules/db-abstract/lib/authors.js
@@ -33,8 +33,8 @@ authors.getInfo = function(authorName, callback) {
 
     db.view('articles/author_info', query,
             function (err, res) {
-                res = db.image.dereferenceDocumentImages(res);
-                callback(err, res);
+                console.log(res);
+                callback(err, _.pluck(res, 'doc'));
             });
 };
 

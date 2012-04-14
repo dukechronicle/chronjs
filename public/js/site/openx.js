2cf6a46fec511b307a6bb8a5454b352328dcfdfe
Dean Chen <dean.chen@duke.edu>
2012-04-14 14:42:52 -0400

    Removed debug statement

--- public/js/site/openx.js
+++ public/js/site/openx.js
@@ -1,6 +1,5 @@
 define(["jquery", "libs/jquery.metadata", "libs/jquery.openxtag.min"], function($) {
     $(function() {
-        console.log("test")
         $.openxtag('init', {
             delivery: '/xhrproxy/openx'
         });

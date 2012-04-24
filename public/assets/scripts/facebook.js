b47c6fe05d5a187e3238e3a06aecf703f3905095
Dean Chen <dean.chen@duke.edu>
2012-04-24 12:08:42 -0400

    Reintegrated social reader work in to new master.

--- public/assets/scripts/facebook.js
+++ public/assets/scripts/facebook.js
@@ -9,14 +9,41 @@ window.fbAsyncInit = function() {
 
     FB.Event.subscribe('auth.statusChange', function(response) {
         if (response.authResponse) {
+            var accessToken = response.authResponse.accessToken;
+
+            $('article').append(
+                "<div id='social-share'>" +
+                    "<div class='activities' style='display:none'></div>" +
+                    "<a class='share-button' data-status='off' href='#'>Sharing On</a>" +
+                    "<a class='activity-button' href='#'>" +
+                    "<img class='activity-icon' src='/images/icons/social-activity-icon.png'></img>" +
+                    "</a>" +
+                    "</div>"
+            );
+            updateRecentActivities(accessToken);
+            $('#social-share .activities td.delete').delegate("a", "click", function() {
+                var activityId = $(this).attr("data-id");
+                $('#activity-' + activityId).remove();
+                $.get("/xhrproxy/delete_activity", {activity_id: activityId, access_token: accessToken });
+                return false;
+            });
+            $('#social-share .activity-button').click(function(event) {
+                event.stopPropagation();
+                event.preventDefault();
+                $('#social-share .activities').toggle();
+            });
             if (location.pathname.match("/article/(.*)")) {
                 setTimeout(function() {
                     var url = "http://www.dukechronicle.com" + location.pathname;
                     $.post(
                         "https://graph.facebook.com/me/dukechronicle:read",
-                        {article: url, access_token: response.authResponse.accessToken}
+                        {article: url, access_token: response.authResponse.accessToken},
+                        function() {
+                            updateRecentActivities(accessToken);
+                        },
+                        "json"
                     )
-                }, 10000)
+                }, 2000)
             }
         }
     });
@@ -31,7 +58,27 @@ window.fbAsyncInit = function() {
 (function(d){
     var js, id = 'facebook-jssdk', ref = d.getElementsByTagName('script')[0];
     if (d.getElementById(id)) {return;}
-js = d.createElement('script'); js.id = id; js.async = true;
-js.src = "//connect.facebook.net/en_US/all.js";
-ref.parentNode.insertBefore(js, ref);
+    js = d.createElement('script'); js.id = id; js.async = true;
+    js.src = "//connect.facebook.net/en_US/all.js";
+    ref.parentNode.insertBefore(js, ref);
 }(document));
+
+function updateRecentActivities(accessToken) {
+    $.getJSON('https://graph.facebook.com/me/dukechronicle:read',
+        {limit: 5, access_token: accessToken},
+        function(data) {
+            var html = "<h4>Recent Activity</h4><table>";
+            var first = "first";
+            _.forEach(data.data, function(row) {html+=
+                "<tr id='activity-" + row.id + "' class='" + first + "'>" +
+                    "<td class='url'><a href='" + row.data.article.url + "'>" + row.data.article.title + "</a></td>" +
+                    "<td class='delete'><a class='delete' data-id='" + row.id + "' href='#'>x</a></td>" +
+                    "</tr>"
+                first = "";
+            });
+            html += "</table>";
+            $('#social-share .activities').empty();
+            $('#social-share .activities').append(html);
+        }
+    );
+}

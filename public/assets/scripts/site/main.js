4bcd58fa7aa0c2a3f7af84f41e86e17c2944c6bd
Jim Posen <jim.posen@gmail.com>
2012-04-24 15:51:41 -0400

    All site Javascript works as it's supposed to, following new convention.

--- public/assets/scripts/site/main.js
+++ public/assets/scripts/site/main.js
@@ -9,11 +9,18 @@ require(['site/align','site/article','site/opinion','site/scrollLoad',
             var args = Array.prototype.slice.call(arguments);
             loadAfterTypekit(args.shift()); // align after typekit loads
             $(function () {
-                for (var i in args)
-                    if (typeof args[i] == 'function')
-                        args[i]();
+                for (var i in args) {
+                    var functions = args[i];
+                    for (var selector in functions)
+                        if (!selector || hasSelector(selector))
+                            functions[selector]();
+                }
             });
         });
+
+function hasSelector(selector) {
+    return $(".js-page-id." + selector).length > 0;
+}
 
 function loadAfterTypekit(callback) {
     if ($('html').hasClass("wf-active") || $('html').hasClass("wf-inactive")) {

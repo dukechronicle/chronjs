ccc77e51b9d09914b943888f811fd752a2fa81de
Jim Posen <jim.posen@gmail.com>
2012-04-30 18:20:21 -0400

    Moved all sports slideshow css into slideshow/style.styl. Everything works again.

--- public/assets/scripts/site/slideshow/frontpage-slideshow.js
+++ public/assets/scripts/site/slideshow/frontpage-slideshow.js
@@ -1,9 +1,13 @@
 define(["jquery", "galleria/galleria-1.2.5"], function($) {
 
+    var GALLERIA_THEME = '/js/galleria/themes/frontpage-slideshow/galleria.frontpage-slideshow.js';
+
+
     return function() {
-        if ($(".slideshow.vertical").length > 0) {
-            Galleria.loadTheme('/js/galleria/themes/frontpage-slideshow/galleria.frontpage-slideshow.js');
-            $(".slideshow.vertical #slides").galleria({
+        if ($(".slideshow.galleria").length > 0) {
+            Galleria.loadTheme(GALLERIA_THEME);
+
+            $(".slideshow.galleria #slides").galleria({
                 transition: "fade",
                 autoplay: 9000,
                 width: 636,

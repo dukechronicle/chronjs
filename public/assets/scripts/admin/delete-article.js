9df70e0c23221aa6f0bd099a7abf450c930ca8e2
Jim Posen <jim.posen@gmail.com>
2012-04-30 22:46:51 -0400

    delete-article.js and delete-image.js use new system.

--- public/assets/scripts/admin/delete-article.js
+++ public/assets/scripts/admin/delete-article.js
@@ -1,45 +1,48 @@
 define(['jquery', 'libs/jquery-ui'], function ($) {
 
-    return { "": deleteArticle }
+    return {
 
-    function deleteArticle() {
+        ".delete-article": function () {
 
-        $(".delete-article").click(function(event) {
-            event.preventDefault();
+            $(".delete-article").click(function(event) {
+                event.preventDefault();
 
-            var anchor = $(event.target);
-            var title = anchor.data('title');
-            var docId = anchor.data('docid');
-            var docRev = anchor.data('rev');
-            var dest = anchor.data('dest');
+                var anchor = $(event.target);
+                var title = anchor.data('title');
+                var docId = anchor.data('docid');
+                var docRev = anchor.data('rev');
+                var dest = anchor.data('dest');
 
-            $('<div id="dialog-confirm">Delete <strong>' + title + '?</strong></div>').appendTo('body');
-
-            $("#dialog-confirm").dialog({
-                dialogClass: 'delete-confirmation-dialog',
-                title: 'Delete Article',
-                resizable: false,
-                autoOpen: true,
-                height:140,
-                modal: true,
-                buttons: {
-                    "Delete": function() {
-
-                        $.ajax({
-                            type: 'DELETE',
-                            url: '/api/article/' + docId,
-                            data: 'rev=' + docRev,
-                            success: function() {
-                                window.location = dest;
-                            }
-                        });
-                    },
-                    Cancel: function() {
-                        $(this).remove();
+                $(createConfirm(title)).appendTo('body').dialog({
+                    dialogClass: 'delete-confirmation-dialog',
+                    title: 'Delete Article',
+                    resizable: false,
+                    autoOpen: true,
+                    height:140,
+                    modal: true,
+                    buttons: {
+                        Delete: function() {
+                            $.ajax({
+                                type: 'DELETE',
+                                url: '/api/article/' + docId,
+                                data: 'rev=' + docRev,
+                                success: function() {
+                                    window.location = dest;
+                                }
+                            });
+                        },
+                        Cancel: function() {
+                            $(this).remove();
+                        }
                     }
-                }
+                });
             });
+        }
 
-        });
-    };
+    }
+
+    function createConfirm(title) {
+        return '<div id="dialog-confirm">Delete <strong>' + title + '?</strong></div>';
+    }
+
 });

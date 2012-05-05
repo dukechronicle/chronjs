c7a00e82b15de1e4a6a0e80a49e35f4bb7a7fc9f
Jim Posen <jim.posen@gmail.com>
2012-05-05 16:04:38 -0400

    Made main.styl for admin pages to test system.

--- build-assets.js
+++ build-assets.js
@@ -12,7 +12,7 @@ var api = require('./thechronicle_modules/api');
 var config = require('./thechronicle_modules/config');
 var log = require('./thechronicle_modules/log');
 
-var STYLE_DIR = __dirname + '/public/assets/styles/';
+var STYLE_DIR = __dirname + '/views/styles/';
 var DIST_DIR = __dirname + '/public/dist/';
 var JS_SOURCES = [ 'site', 'admin' ];
 

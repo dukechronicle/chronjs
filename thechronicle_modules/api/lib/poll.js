3c01129fa2307dc63b9e42e6131e903c04876d15
Jim Posen <jim.posen@gmail.com>
2012-05-30 21:49:23 -0400

    Redesigned part of poll api. Needs testing.

--- thechronicle_modules/api/lib/poll.js
+++ thechronicle_modules/api/lib/poll.js
@@ -6,46 +6,32 @@ var log = require('../../log');
 
 var _ = require('underscore');
 
-poll.add = function(fields, callback) {
+poll.add = function (fields, callback) {
+    if (!fields.title) return callback("Poll must have title");
+    var answers = _.reduce(fields.answers, function (memo, answer) {
+        memo[answer] = 0;
+        return memo;
+    }, {});
     var poll = {
         title: fields.title,
         taxonomy: fields.taxonomy,
         created: util.unixTimestamp(),
         type: 'poll',
-        answers: _.reduce(fields.answers,
-                          function (memo, answer) {
-                          	if (answer) {
-                              memo[answer] = 0;
-                             }
-                              return memo;
-                          }, {})
+        answers: answers
     };
     db.poll.add(poll, callback);
 };
 
-poll.edit = function(fields, callback) {
-	var answersWithCounts = {};
-	for (i=0; i<fields.answers.length;i++) {
-		if (fields.answers[i]) {
-		    answersWithCounts[fields.answers[i]] = parseInt(fields.count[i]);
-		}
-	}
-	var poll = {
-        title: fields.title,
-        taxonomy: fields.taxonomy,
-        created: util.unixTimestamp(),
-        type: 'poll',
-        answers: answersWithCounts
-    };
+poll.edit = function (fields, callback) {
     db.poll.edit(fields.id, poll, callback);
 }
 
 poll.vote = function (id, answer, callback) {
     db.poll.getPoll(id, function (err, doc) {
-        if (err)
-            callback(err);
-        else if (! (answer in doc.answers))
+        if (err) callback(err);
+        else if (! (answer in doc.answers)) {
             callback(answer + ' is not an option for poll ' + id);
+        }
         else {
             doc.answers[answer]++;
             db.poll.edit(id, doc, callback);
@@ -59,43 +45,38 @@ poll.setSection = function (id, taxonomy, callback) {
 
 poll.getPoll = function (id, callback) {
     db.poll.getPoll(id, function (err, res) {
-        if (err)
-            callback(err);
-        else if (res.length == 0)
+        if (err) callback(err);
+        else if (res.length == 0) {
             callback('No poll found with id: ' + id);
-        else
-            callback(null, res);
+        }
+        else callback(null, res);
     });
 };
 
-poll.getBySection = function (taxonomy, limit, callback) {
-    var query = {};
-    if (limit != undefined) query.limit = limit;
-    db.poll.getBySection(taxonomy, query, function (err, res) {
-        if (err) callback(err);
-        else callback(null, _.map(res, function (doc) { return doc.value }));
-    });
+poll.getByTaxonomy = function (taxonomy, limit, callback) {
+    taxonomy = taxonomy || [];
+    db.poll.getBySection(taxonomy, limit, callbackDocumentValues(callback));
 };
 
 poll.getByTitle = function (title, callback) {
-    db.poll.getByTitle(title, {}, function (err, res) {
+    db.poll.getByTitle(title, function (err, res) {
         if (err) callback(err);
-        else callback(null, _.map(res, function (doc) { return doc.value }));
+        else if (res.length == 0) callback("No polls found");
+        else callback(null, res[0].value);
     });
 };
 
-poll.getByVotes = function (descending, query, callback) {
-    query = query || {};
-    if (descending) query.desending = true;
-    db.poll.getByVotes(query, function (err, res) {
-        if (err) callback(err);
-        else callback(null, _.map(res, function (doc) { return doc.value }));
-    });
+poll.getByVotes = function (descending, limit, callback) {
+    db.poll.getByVotes(descending, limit, callbackDocumentValues(callback));
 };
 
-poll.getByDate = function (query, callback) {
-    db.poll.getByDate(query, function (err, res) {
+poll.getByDate = function (limit, callback) {
+    poll.getByTaxonomy(null, limit, callback);
+};
+
+function callbackDocumentValues(callback) {
+    return function (err, res) {
         if (err) callback(err);
         else callback(null, _.map(res, function (doc) { return doc.value }));
-    });
-};
+    }
+}

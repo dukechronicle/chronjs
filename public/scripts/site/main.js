e707874224461ffd0836a1d438daf264ee0a1d53
Jim Posen <jim.posen@gmail.com>
2012-05-31 00:45:09 -0400

    Poll style and voting stuff is done.

--- public/scripts/site/main.js
+++ public/scripts/site/main.js
@@ -4,7 +4,7 @@ require.config({
 
 require(['site/align','site/article','site/category-box','site/scrollLoad',
          'site/slideshow/frontpage-slideshow','site/slideshow/slideshow-right',
-         'site/openx', 'libs/jquery.cookie', 'facebook'],
+         'site/openx', 'site/poll', 'libs/jquery.cookie', 'facebook'],
         function () {
             var args = Array.prototype.slice.call(arguments);
             $(function () {

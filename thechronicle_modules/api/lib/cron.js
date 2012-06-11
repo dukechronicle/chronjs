b9eb624bd89ba339548ea1a60423d40f9dff082b
Jim Posen <jim.posen@gmail.com>
2012-06-10 20:18:18 -0400

    cron jobs weren't executing and blog feeds weren't being read correctly.

--- thechronicle_modules/api/lib/cron.js
+++ thechronicle_modules/api/lib/cron.js
@@ -3,42 +3,47 @@ var rss = require('./rss');
 var log = require('../../log');
 var config = require('../../config');
 
-var madeInitialCronJobs = false;
+var initialized = false;
+
 
 exports.init = function () {  
-    if (!madeInitialCronJobs) {
-        madeInitialCronJobs = true;
+    if (!initialized) {
+        initialized = true;
 
-        new cron.CronJob('0 */30 * * * *', function () { //every 30 minutes
-            
-            // in a multi-instance setup, it may be possible that the config was changed on 1 instance, and those changes should replicate to all instances            
-            config.checkForUpdatedConfig(function(updated) {
-                 if(updated) {
-                    log.notice("Config updated to use revision " + config.getConfigRevision());
-                    config.runAfterConfigChangeFunction(function (err) {
-                        if (err) log.error(err);
-                    });
-                }
-            });            
+        // In a multi-instance setup, it may be possible that the config was
+        // changed on one instance, and those changes should replicate to all
+        // instances. Runs every half hour.
+        new cron.CronJob('0 0,30 * * * *', loadConfiguration).start();
 
-            if(process.env.NODE_ENV === 'production') {
-                config.get('RSS_FEEDS').forEach(function (feed) {
-                    rss.parseRSS(feed.url, function (err, dom) {
-                        log.notice("Parsed RSS for feed: " + feed.title);
-                        if (err) log.warning(err);
-                        else {
-                            rss.storeRSS(dom, feed.title, function (err, res) {
-                                if (err) log.warning(err);
-                                else log.notice("Stored RSS for feed: " + feed.title);
-                            });
-                        }
-                    });
-                });
-            }
-        });
+        if(process.env.NODE_ENV === 'production') {
+            new cron.CronJob('0 * * * * *', loadRSSFeeds).start();
+        }
     }
 };
 
-exports.CronJob = function(whenToRunString, funcToRun) {
-    return new cron.CronJob(whenToRunString, funcToRun);
-};
+
+function loadConfiguration() {
+    config.checkForUpdatedConfig(function(updated) {
+        if(updated) {
+            log.notice("Config updated to use revision " + config.getConfigRevision());
+            config.runAfterConfigChangeFunction(function (err) {
+                if (err) log.error(err);
+            });
+        }
+    });
+}
+
+function loadRSSFeeds() {
+    config.get('RSS_FEEDS').forEach(function (feed) {
+        rss.parseRSS(feed.url, function (err, dom) {
+            log.notice("Parsed RSS for feed: " + feed.title);
+            if (err) log.warning(err);
+            else {
+                rss.storeRSS(dom, feed.title, function (err, res) {
+                    if (err) log.warning(err);
+                    else log.notice("Stored RSS for feed: " + feed.title);
+                });
+            }
+        });
+    });
+}

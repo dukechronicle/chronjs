225d6afbc9221977d949da4714ab83cf4b789c5a
Jim Posen <jim.posen@gmail.com>
2012-07-08 16:34:34 -0400

    Made build module, which will be different from build-assets cli tool.

--- build-assets.js
+++ build-assets.js
@@ -1,19 +1,15 @@
 var async = require('async');
-var cleanCSS = require('clean-css');
-var crypto = require('crypto');
-var requirejs = require('requirejs');
-var fs = require('fs');
-var gzip = require('gzip');
+
 var pathutil = require('path');
-var stylus = require('stylus');
-var walk = require('walk');
+
+
 var _ = require('underscore');
 
 var api = require('./thechronicle_modules/api');
+var build = require('./thechronicle_modules/build');
 var config = require('./thechronicle_modules/config');
 var log = require('./thechronicle_modules/log');
 
-var STATIC_BUCKET = null;
 var STYLE_DIR = __dirname + '/views/styles/';
 var DIST_DIR = __dirname + '/public/dist/';
 var PUBLIC_DIR = __dirname + '/public/';
@@ -27,6 +23,7 @@ async.waterfall([
     function (callback) {
         if (config.isSetUp()) {
             STATIC_BUCKET = config.get('S3_STATIC_BUCKET');
+            build.init(__dirname);
             api.init(callback);
         }
         else {
@@ -41,7 +38,10 @@ async.waterfall([
     }
     */
     function (callback) {
-        pushSourceDirectory(pathutil.resolve('public/css'), callback);
+        build.buildAllCSS(function (err, res) {
+            if (err) callback(err);
+            else log.debug(res);
+        });
     }
 ], function (err) {
     if (err) log.error(err);
@@ -62,41 +62,7 @@ function pushAssets(paths, callback) {
     });
 }
 
-function pushSourceDirectory(dir, callback) {
-    var errors = [];
-    var walker = walk.walk(dir);
 
-    walker.on('file', function (root, stats, next) {
-        var filepath = pathutil.join(root, stats.name);
-        log.debug(filepath);
-        pushSourceFile(filepath, function (err) {
-            if (err) {
-                errors.push('Error pushing ' + filepath + ': ' + err);
-            }
-            next();
-        });
-    });
-
-    walker.on('end', function () {
-        if (errors.length == 0) {
-            errors = null;
-        }
-        callback(errors);
-    });
-}
-
-function pushSourceFile(filepath, callback) {
-    var key = pathutil.relative(PUBLIC_DIR, filepath);
-    var type = getMimeType(pathutil.extname(filepath));
-    if (!type) {
-        return callback('Cannot determine file type: ' + path);
-    }
-
-    fs.readFile(filepath, function (err, data) {
-        if (err) return callback(err);
-        api.s3.put(STATIC_BUCKET, data, key, type, null, callback);
-    });
-}
 
 function pushAll(paths, type) {
     return function (callback) {
@@ -115,106 +81,4 @@ function pushAll(paths, type) {
             });
         }, callback);
     };
-}
-
-function buildCSS(callback) {
-    var paths = {};
-    fs.readdir(STYLE_DIR, function (err, files) {
-        async.forEachSeries(files, function (file, cb) {
-            fs.stat(STYLE_DIR + file, function (err, stats) {
-                if (err) cb(err);
-                else if (stats.isDirectory())
-                    buildCSSFile(file, function (err, path) {
-                        paths[file] = path;
-                        cb(err);
-                    });
-                else cb();
-            });
-        }, function (err) {
-            callback(err, paths);
-        });
-    });
-}
-
-function buildCSSFile(path, callback) {
-    var filepath = STYLE_DIR + path + '/main.styl';
-    fs.readFile(filepath, function (err, contents) {
-        if (err) return callback(err);
-
-        var renderer = stylus(contents.toString())
-            .set('filename', filepath)
-            .set('compress', true)
-            .set('include css', true);
-        renderer.render(function(err, data) {
-            if (err) {
-                log.error(err);
-                return callback(err);
-            }
-
-            var style = cleanCSS.process(data);
-            fs.writeFile(DIST_DIR + path + '.css', style, function (err) {
-                callback(err, '/dist/' + path + '.css');
-            });
-        });
-    });
-}
-
-function buildJavascript(callback) {
-    var paths = {};
-    async.forEachSeries(JS_SOURCES, function (src, cb) {
-        buildJavascriptFile(src, function (err, path) {
-            paths[src] = path;
-            cb(err);
-        });
-    }, function (err) {
-        callback(err, paths);
-    });
-}
-
-function buildJavascriptFile(src, callback) {
-    var config = {
-        baseUrl: 'public/scripts',
-        name: src + '/main',
-        out: 'public/dist/' + src + '.js',
-        paths: {
-            jquery: 'require-jquery'
-        }
-    };
-
-    requirejs.optimize(config, function (buildResponse) {
-        callback(null, '/dist/' + src + '.js');
-    });
-}
-
-function storeS3(data, type, callback) {
-    var md5sum = crypto.createHash('md5');
-    md5sum.update(data);
-    var path = '/dist/' + md5sum.digest('hex');
-
-    gzip(data, function (err, buffer) {
-        if (err) return callback(err);
-        api.s3.put(STATIC_BUCKET, buffer, path, type, "gzip", function(err) {
-            callback(err, config.get('CLOUDFRONT_STATIC') + path);
-        });
-    });
-}
-
-function getMimeType(extension) {
-    var extensionToType = {
-        '': 'text/plain',
-        '.css': 'text/css',
-        '.eot': 'application/vnd.ms-fontobject',
-        '.gif': 'image/gif',
-        '.html': 'text/html',
-        '.ico': 'image/x-icon',
-        '.jpg': 'image/jpeg',
-        '.js': 'application/javascript',
-        '.otf': 'application/octet-stream',
-        '.pdf': 'application/pdf',
-        '.png': 'image/png',
-        '.svg': 'image/svg+xml',
-        '.ttf': 'application/octet-stream',
-        '.woff': 'application/font-woff',
-    }
-    return extensionToType[extension];
 }

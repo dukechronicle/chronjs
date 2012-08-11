721d1a2af2719b526bfea3bc4c35b55e60544f6b
Jim Posen <jim.posen@gmail.com>
2012-08-11 12:20:38 -0400

    Rewrote api.taxonomy module with tests.

--- test/taxonomy.js
+++ test/taxonomy.js
@@ -1,20 +1,85 @@
 var api = require('../thechronicle_modules/api');
 
+var should = require('chai').should();
+var util = require('util');
+
 
 describe('api.taxonomy', function () {
-    describe('#isValid()', function () {
-        it('should be false if taxonomy is not valid', function () {
-            api.taxonomy.isValid(['sports', 'fakesport'])
-                .should.not.be.ok;
+
+    before(init);
+
+    describe('#getTaxonomy', function () {
+        it('should be undefined if taxonomy is not valid', function () {
+            should.not.exist(api.taxonomy.getTaxonomy(['sports', 'fakesport']));
         });
 
-        it('should return canonical taxonomy if valid', function () {
-            api.taxonomy.isValid(['news', 'local & national'])
-                .should.equal(['News', 'Local & National']);
+        it('should be a root node if empty', function () {
+            api.taxonomy.getTaxonomy([]).should.deep.equal({
+                taxonomy: [],
+                path: '',
+            });
+        });
+
+        it('should return taxonomy node if valid', function () {
+            api.taxonomy.getTaxonomy(['news', 'health & science', 'duhs'])
+                .should.deep.equal({
+                    name: 'DUHS',
+                    taxonomy: ['News', 'Health & Science', 'DUHS'],
+                    path: 'news/health & science/duhs',
+                });
         });
     });
 
-    //describe('#getTaxonomyPath()', );
-    //describe('#getParents()', );
-    //describe('#getChildren()', );
+    describe('#children()', function () {
+        it('should return an array of child nodes', function () {
+            api.taxonomy.children(['sports', 'basketball'])
+                .should.deep.equal([
+                    {
+                        name: 'Men',
+                        taxonomy: ['Sports', 'Basketball', 'Men'],
+                        path: 'sports/basketball/men',
+                    },
+                    {
+                        name: 'Women',
+                        taxonomy: ['Sports', 'Basketball', 'Women'],
+                        path: 'sports/basketball/women',
+                    },
+                ]);
+        });
+    });
+
+    describe('#parents()', function () {
+        it('should return an array of parent nodes', function () {
+            api.taxonomy.parents(['sports', 'basketball', 'men'])
+                .should.deep.equal([
+                    {
+                        name: 'Sports',
+                        taxonomy: ['Sports'],
+                        path: 'sports',
+                    },
+                    {
+                        name: 'Basketball',
+                        taxonomy: ['Sports', 'Basketball'],
+                        path: 'sports/basketball',
+                    },
+                ]);
+        });
+    });
+
+    describe('#isValid()', function () {
+        it('should be true if taxonomy exists', function () {
+            api.taxonomy.isValid(['sports', 'basketball']).should.be.true;
+        });
+
+        it('should be undefined if taxonomy does not exist', function () {
+            api.taxonomy.isValid(['sports', 'fakesport']).should.be.false;
+        });
+    });
+
+    describe('#getMainSections()', function () {
+        it('should return names of top level sections', function () {
+            var sections = ['News', 'Sports', 'Opinion', 'Recess', 'Towerview'];
+            api.taxonomy.mainSections().should.deep.equal(sections);
+        });
+    });
 });
--- thechronicle_modules/api/lib/taxonomy.js
+++ thechronicle_modules/api/lib/taxonomy.js
@@ -9,68 +9,103 @@ var _ = require('underscore');
 var async = require('async');
 
 
-taxonomy.getTaxonomyTree = function(taxonomyPath) {
-    var tree = config.get("TAXONOMY");
-    if (taxonomyPath) {
-        _.each(taxonomyPath, function (section) {
-            try {
-                tree = tree[section];
-            } catch (err) {
-                tree = undefined;
-            }
+/**
+ * Finds the taxonomy if it exists and returns a taxonomy node object.
+ *
+ * @param {Array.<string>} taxonomy The taxonomy array.
+ * @return {Object} The taxonomy node object with name, taxonomy, and path
+ *     properties or undefined if taxonomy does not exist.
+ */
+taxonomy.getTaxonomy = function (tax) {
+    var node = getTaxonomyNode(tax);
+    if (node) {
+        delete node.children;
+        return node;
+    }
+    return undefined;
+};
+
+/**
+ * Get the child taxonomy nodes.
+ *
+ * @param {Array.<string>} taxonomy The taxonomy array.
+ * @return {Array.<Object>} An array of child taxonomy nodes in order.
+ */
+taxonomy.children = function (tax) {
+    var node = getTaxonomyNode(tax);
+    if (node) {
+        return _.map(node.children, function (child) {
+            var fullTaxonomy = _.clone(node.taxonomy);
+            fullTaxonomy.push(child.name);
+            child = _.extend(child, {
+                taxonomy: fullTaxonomy,
+                path: taxonomy.path(fullTaxonomy),
+            });
+            delete child.children;
+            return child;
         });
     }
-    return tree;
+    return undefined;
 };
 
-taxonomy.getParents = function(taxonomyPath, callback) {
-    var parents = [];
-    var taxonomyPath = _.clone(taxonomyPath);
-    while (taxonomyPath.length > 1) {
-        var parentName = taxonomyPath.pop();
-        parents.push({path:'/' + taxonomyPath.join('/'), name:parentName});
+/**
+ * Get the parent taxonomy nodes.
+ *
+ * @param {Array.<string>} taxonomy The taxonomy array.
+ * @return {Array.<Object>} An array of parent taxonomy nodes in order.
+ */
+taxonomy.parents = function (tax) {
+    var node = getTaxonomyNode(tax);
+    if (node) {
+        tax = node.taxonomy;
+        tax.pop();
+        var parents = [];
+        while (tax.length > 0) {
+            parents.unshift({
+                name: _.last(tax),
+                taxonomy: _.clone(tax),
+                path: taxonomy.path(tax),
+            });
+            tax.pop();
+        }
+        return parents;
     }
-    callback(null, parents.reverse());
+    return undefined;
 };
 
-taxonomy.getChildren = function(taxonomyPath, callback) {
-    var path = taxonomyPath.join('/');
-    var tree = taxonomy.getTaxonomyTree(taxonomyPath);
-
-    if (!tree) return callback("Taxonomy not valid: " + path);
-
-    var children = _.map(tree, function (key, value) {
-        var child = {};
-        child[path + '/' + value] = value;
-        return child;
+/**
+ * Get a list of the names of the top level taxonomy sections.
+ *
+ * @return {Array.<string>} The names of the main sections.
+ */
+taxonomy.mainSections = function () {
+    return _.map(taxonomy.children([]), function (node) {
+        return node.name;
     });
-    callback(null, children);
 };
 
-taxonomy.getTaxonomyListing = function (callback) {
-    var tree = taxonomy.getTaxonomyTree();
-    var sections = {};
-    _.forEach(tree, function (value, key) {
-        var listing = {}, path = [key];
-        listing[JSON.stringify(path)] = key;
-        getTaxonomyListingHelper(value, listing, path, 1);
-        sections[key] = listing;
-    });
-    callback(null, sections);
+/**
+ * Check if a taxonomy is valid.
+ *
+ * @param {Array.<string>} taxonomy The taxonomy array.
+ * @return {boolean} Whether taxonomy exists.
+ */
+taxonomy.isValid = function (tax) {
+    return new Boolean(taxonomy.getTaxonomy(tax));
 };
 
-function getTaxonomyListingHelper(tree, listing, path, depth) {
-    _.forEach(tree, function (value, key) {
-        var dashes = "";
-        for (var i = 0; i < depth; i++)
-            dashes += "-";
-        path.push(key);
-        listing[JSON.stringify(path)] = dashes + "  " + key;
-        getTaxonomyListingHelper(value, listing, path, depth + 1);
-        path.pop();
-    });
-    return listing;
-}
+/**
+ * Convert a taxonomy into a string path. The path is the lowercased taxonomy
+ * components concatenated with '/'.
+ *
+ * @param {Array.<string>} taxonomy The taxonomy array.
+ * @return {string} The taxonomy path.
+ */
+taxonomy.path = function (tax) {
+    return _.map(tax, function (section) {
+        return section.toLowerCase();
+    }).join('/');
+};
 
 taxonomy.getHierarchy = function (callback) {
     db.taxonomy.getHierarchy(function (err, res) {
@@ -94,3 +129,49 @@ taxonomy.getHierarchyTree = function (callback) {
         callback(null, root);
     });
 };
+
+taxonomy.getTaxonomyListing = function (callback) {
+    var tree = taxonomy.getTaxonomyTree();
+    var sections = {};
+    _.forEach(tree, function (value, key) {
+        var listing = {}, path = [key];
+        listing[JSON.stringify(path)] = key;
+        getTaxonomyListingHelper(value, listing, path, 1);
+        sections[key] = listing;
+    });
+    callback(null, sections);
+};
+
+function getTaxonomyNode(tax) {
+    var root = {children: config.get('TAXONOMY')};
+    var fullTaxonomy = [];
+
+    for (var i = 0; i < tax.length; i++) {
+        root = _.find(root.children || [], function (child) {
+            return child.name.toLowerCase() === tax[i].toLowerCase();
+        });
+
+        if (!root) {
+            return null;
+        }
+        fullTaxonomy.push(root.name);
+    }
+
+    return _.extend(root, {
+        taxonomy: fullTaxonomy,
+        path: taxonomy.path(fullTaxonomy),
+    });
+};
+
+function getTaxonomyListingHelper(tree, listing, path, depth) {
+    _.forEach(tree, function (value, key) {
+        var dashes = "";
+        for (var i = 0; i < depth; i++)
+            dashes += "-";
+        path.push(key);
+        listing[JSON.stringify(path)] = dashes + "  " + key;
+        getTaxonomyListingHelper(value, listing, path, depth + 1);
+        path.pop();
+    });
+    return listing;
+}

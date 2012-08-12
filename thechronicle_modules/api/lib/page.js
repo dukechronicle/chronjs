1b57d2a182742a2f7d815837935f79f773f0ae70
Jim Posen <jim.posen@gmail.com>
2012-08-12 11:03:31 -0400

    Made schemata for pages as variable in pages module.

--- thechronicle_modules/api/lib/page.js
+++ thechronicle_modules/api/lib/page.js
@@ -4,6 +4,24 @@ var api = require("../../api");
 var db = require("../../db-abstract");
 var log = require("../../log");
 
+var md = require('discount');
+var _ = require('underscore');
+
+
+var GROUP_TYPES = {
+    'markdown': md.parse,
+};
+
+page.schemata = {
+    'Single Block': {
+        view: 'site/pages/single-block',
+        model: {
+            contents: 'markdown',
+        }
+    }
+};
+
+
 page.getByUrl = function (url, callback) {
     db.page.getByUrl(url, function (err, res) {
         if (err) callback(err);
@@ -11,6 +29,19 @@ page.getByUrl = function (url, callback) {
         else if (res.length == 1) callback(null, res[0].value);
         else callback('Multiple pages found with url: ' + url, res);
     });
+};
+
+page.generateModel = function (page) {
+    var template = page.schemata[page.template].model;
+    var model = {};
+    _.each(template, function (value, key) {
+        model[key] = GROUP_TYPES[value](page.model[key]);
+    });
+    return model;
+};
+
+page.view = function (page) {
+    return page.schemata[page.template].view;
 };
 
 page.add = function (data, callback) {

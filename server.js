e74f21d5898ed7bf81d4afedc0a02fb5bf89f2d3
Jim Posen <jim.posen@gmail.com>
2012-08-20 01:25:16 -0400

    I'm an idiot.

--- server.js
+++ server.js
@@ -143,7 +143,7 @@ function newServer() {
 
 function configureVirtualHosts() {
     app.configure(function () {
-        var siteDomainName = 'localhost'; //config.get('DOMAIN_NAME');
+        var siteDomainName = config.get('DOMAIN_NAME');
         var mobileDomainName = config.get('MOBILE_DOMAIN_NAME');
         app.use(express.vhost(siteDomainName, route.siteInit(newServer())));
         app.use(express.vhost(mobileDomainName, route.mobileInit(newServer())));

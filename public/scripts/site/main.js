7a7eff3a9587654463c696a052a4032bc574d6b4
Jim Posen <jim.posen@gmail.com>
2012-08-21 17:10:30 -0400

    Rewrote align.js in coffeescript.

--- public/scripts/site/main.js
+++ public/scripts/site/main.js
@@ -2,9 +2,10 @@ require.config({
     baseUrl: '/scripts'
 });
 
-require(['cs!common/main', 'site/align','cs!site/article','site/category-box','site/scrollLoad',
-         'site/slideshow/frontpage-slideshow','site/slideshow/slideshow-right',
-         'cs!site/openx', 'site/poll', 'lib/jquery.cookie', 'site/facebook'],
+require(['cs!common/main','cs!site/align','cs!site/article','site/category-box',
+         'site/scrollLoad','site/slideshow/frontpage-slideshow','site/poll',
+         'site/slideshow/slideshow-right','cs!site/openx',  'lib/jquery.cookie',
+         'site/facebook'],
         function (main) {
             main.apply(this, arguments);
         });

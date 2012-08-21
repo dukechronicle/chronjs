db00995826146ea7f9c61fee4aaa06d6d2d8d5f3
Jim Posen <jim.posen@gmail.com>
2012-08-21 16:21:56 -0400

    Rewrote taxonomy.js and form-field.js in coffeescript.

--- public/scripts/admin/main.js
+++ public/scripts/admin/main.js
@@ -4,8 +4,8 @@ require.config({
 
 require(['cs!common/main','admin/crop','admin/delete-article',
          'admin/delete-image','admin/json-to-form','admin/k4export',
-         'admin/layout','admin/newsletter','admin/nicedate','admin/form-field',
-         'admin/taxonomy','admin/html5upload'],
+         'admin/layout','admin/newsletter','admin/nicedate',
+         'cs!admin/form-field','cs!admin/taxonomy','admin/html5upload'],
         function (main) {
             main.apply(this, arguments);
         });

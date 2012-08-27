fd583ae168e044b47b31f11a5d9ae3f41e3f8d9c
Jim Posen <jim.posen@gmail.com>
2012-08-27 11:32:35 -0400

    Cleaned some stuff up in the newsletter api, but need to update the API calls.

--- thechronicle_modules/api/lib/newsletter.js
+++ thechronicle_modules/api/lib/newsletter.js
@@ -1,108 +1,91 @@
-var newsletter = {};
-var exports = module.exports = newsletter;
+var newsletter = exports;
 
-var MailChimpAPI = require('mailchimp').MailChimpAPI;
-var api = require('./api');
-var _ = require('underscore');
 var async = require('async');
-var log = require('../../log');
-var jade = require('jade');
+var dateFormat = require('dateformat');
 var fs = require('fs');
+var jade = require('jade');
+var mailchimp = require('mailchimp');
+var _ = require('underscore');
+
+var api = require('./api');
+var log = require('../../log');
 var config = require('../../config');
 
-var apiKey = null;
-var layoutGroups = null;
-var listID = null;
-var templateID = null;
 var mcAPI = null;
 
 var ARTICLE_IMAGE_WIDTH = 124;
 var ARTICLE_IMAGE_HEIGHT = 89;
+var NEWSLETTER_FROM_EMAIL;
+var NEWSLETTER_FROM_NAME = 'The Chronicle';
 
-var newsletterFromEmail = null;
-var newsletterFromName = "The Chronicle";
 
 newsletter.init = function() {
-    apiKey = config.get("MAILCHIMP_API_KEY");
-    layoutGroups = config.get("LAYOUT_GROUPS");
-    listID = config.get("MAILCHIMP_LIST_ID");
+    NEWSLETTER_FROM_EMAIL = "no-reply@"+config.get('DOMAIN_NAME').replace("www.", "");
 
-    templateID = config.get("MAILCHIMP_TEMPLATE_ID");
-
-    newsletterFromEmail = "no-reply@"+config.get('DOMAIN_NAME').replace("www.", "");
-
-    try { 
-        mcAPI = new MailChimpAPI(apiKey, { version : '1.3', secure : false });
+    var apiKey = config.get("MAILCHIMP_API_KEY");
+    try {
+        mcAPI = new mailchimp.MailChimpAPI(apiKey, {version : '1.3', secure : true});
     } catch (error) {
-        log.warning(error);
+        log.error(error);
     }
 };
 
-function getNewsletterSubject() {
-    return "Duke Chronicle Daily Newsletter " + getDate();
-}
-
-function getDate() {
-    var today = new Date();
-    var dd = today.getDate();
-    var mm = today.getMonth()+1;//January is 0!
-    var yyyy = today.getFullYear();
-    if(dd<10){
-        dd='0'+dd;
-    }   
-    if(mm<10){
-        mm='0'+mm;
-    }  
-    return mm+'/'+dd+'/'+yyyy;
-}
-
-newsletter.sendTestNewsletter = function(campaignID, emailToSendTo, callback) {
-    var params = {"test_emails":[emailToSendTo], "cid":campaignID};
-    mcAPI.campaignSendTest(params, function (res) {
-        if (res === false) {
-            log.warning("Sending Campaign failed!");
-            callback("Sending Campaign failed!");
-        }
+newsletter.sendTestNewsletter = function (campaignID, emailToSendTo, callback) {
+    var options = {
+        test_emails: [emailToSendTo],
+        cid: campaignID
+    };
+    mcAPI.campaignSendTest(options, function (res) {
+        if (!res) callback("Sending Campaign failed!");
         else callback();
     });
 };
 
 newsletter.addSubscriber = function (subscriberEmail, callback) {
-    var params = {"id":listID, "email_address":subscriberEmail, "send_welcome":true};
-    mcAPI.listSubscribe(params, function (res) {
-        if (res === false) {
-            log.warning("Adding subscriber to list failed!");
-            return callback("Adding subscriber to list failed!");
-        }
-        callback(null);
+    var options = {
+        id: config.get('MAILCHIMP_LIST_ID'),
+        email_address: subscriberEmail,
+        send_welcome: true
+    };
+    mcAPI.listSubscribe(options, function (res) {
+        if (!res) callback("Adding subscriber to list failed!");
+        else callback();
     });
 };
 
 newsletter.removeSubscriber = function (subscriberEmail, callback) {
-    var params = {"id":listID, "email_address":subscriberEmail, "delete_member":true, "send_welcome":true, "send_goodbye":false};
-    mcAPI.listUnsubscribe(params, function (res) {
-        if (res === false) {
-            log.warning("Removing subscriber to list failed!");
-            return callback("Removing subscriber to list failed!");
-        }
-        callback(null);
+    var options = {
+        id: config.get('MAILCHIMP_LIST_ID'),
+        email_address: subscriberEmail,
+        delete_member: true,
+        send_welcome: true,
+        send_goodbye: false
+    };
+    mcAPI.listUnsubscribe(options, function (res) {
+        if (!res) callback("Removing subscriber to list failed!");
+        else callback();
     });
 };
 
 newsletter.sendNewsletter = function (campaignID, callback) {
     mcAPI.campaignSendNow({cid:campaignID}, function (res) {
-        if (res === false) {
-            log.warning("Sending Campaign failed!");
-            return callback("Sending Campaign failed!");
-        }
-        return callback(null);
+        if (!res) callback("Sending Campaign failed!");
+        else callback(null);
     });
 };
 
 newsletter.createNewsletter = function (callback) {
-    var optArray = {"list_id":listID, "subject":getNewsletterSubject(), "from_email":newsletterFromEmail, "from_name":newsletterFromName, "title":getNewsletterSubject(), "template_id":templateID};
-    
-    api.group.docs(layoutGroups.Newsletter.namespace, null, function (err, model) {
+    var options = {
+        list_id: listID,
+        subject: getNewsletterSubject(),
+        from_email: newsletterFromEmail,
+        from_name: newsletterFromName,
+        title: getNewsletterSubject(),
+        template_id: config.get('MAILCHIMP_TEMPLATE_ID'),
+    };
+
+    var namespace = config.get('LAYOUT_GROUPS').Newsletter.namespace;
+    api.group.docs(namespace, null, function (err, model) {
         fs.readFile('views/newsletter.jade', function (err, data) {
             var newsHTML = jade.compile(data)({
                 model: model
@@ -117,7 +100,6 @@ newsletter.createNewsletter = function (callback) {
 
             mcAPI.campaignCreate(params, function (res) {
                 if (res.error) {
-                    log.warning('Error: ' + res.error + ' (' + res.code + ')');
                     callback('Error: ' + res.error + ' (' + res.code + ')');
                 }
                 else {
@@ -126,5 +108,13 @@ newsletter.createNewsletter = function (callback) {
                 }
             });
         });
-    });         
+    });
 };
+
+function getNewsletterSubject() {
+    return "Duke Chronicle Daily Newsletter " + getDate();
+}
+
+function getDate() {
+    return dateFormat(newDate, 'm/d/yyyy');
+}

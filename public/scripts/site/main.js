3e85bd66c62a96f25bcbe0b671a8c154629be39e
Jim Posen <jim.posen@gmail.com>
2012-08-31 01:48:55 -0400

    Finished countdown timer.

--- public/scripts/site/main.js
+++ public/scripts/site/main.js
@@ -5,7 +5,7 @@ require.config({
 require(['cs!common/main','cs!site/align','cs!site/article','site/category-box',
          'site/scrollLoad','site/slideshow/frontpage-slideshow','site/poll',
          'site/slideshow/slideshow-right','cs!site/openx',  'lib/jquery.cookie',
-         'site/facebook'],
+         'site/facebook', 'cs!site/countdown'],
         function (main) {
             main.apply(this, arguments);
         });

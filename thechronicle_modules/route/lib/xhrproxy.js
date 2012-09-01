1b54e6c90a9b4c98671e1225ce35b9b7c82c0185
Jim Posen <jim.posen@gmail.com>
2012-09-01 12:26:29 -0400

    Set up an xhrproxy route for ESPN page for score fetching.

--- thechronicle_modules/route/lib/xhrproxy.js
+++ thechronicle_modules/route/lib/xhrproxy.js
@@ -2,6 +2,11 @@ var xhrproxy = exports;
 
 var http = require('http');
 var https = require('https');
+var url = require('url');
+
+var log = require('../../log');
+
+var ESPN_URL = 'http://espn.go.com/college-football/team/_/id/150/duke-blue-devils';
 
 
 xhrproxy.delete_activity = function(req, res, next) {
@@ -10,7 +15,7 @@ xhrproxy.delete_activity = function(req, res, next) {
         host: 'graph.facebook.com',
         port: 443,
         path: path,
-        method: 'DELETE'
+        method: 'DELETE',
         secure: true,
     };
     sendRequest(options, function (err, body) {
@@ -19,8 +24,20 @@ xhrproxy.delete_activity = function(req, res, next) {
     });
 }
 
+xhrproxy.espn = function (req, res, next) {
+    var urlParams = url.parse(ESPN_URL);
+    var options = {
+        host: urlParams.host,
+        path: urlParams.path,
+    };
+    sendRequest(options, function (err, body) {
+        if (err) res.send(err, 500);
+        else res.send(body);
+    });
+};
+
 function sendRequest(options, callback) {
-    var client = secure ? https : http;
+    var client = options.secure ? https : http;
     var req = client.request(options, function (res) {
         var body = '';
         res.setEncoding('utf8');

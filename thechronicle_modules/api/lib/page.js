aa6564c4d63bf535fedcad8b6ec8bb2f32718b92
Jim Posen <jim.posen@gmail.com>
2012-09-04 23:11:19 -0400

    Made top section of election page.

--- thechronicle_modules/api/lib/page.js
+++ thechronicle_modules/api/lib/page.js
@@ -197,9 +197,19 @@ exports.templates = {
         model: {
             articles: {
                 type: 'array',
-                name: 'Event Articles',
+                name: 'Articles',
                 required: true,
                 items: {'$ref': 'article'},
+            },
+            text: {
+                extends: {'$ref': 'markdown'},
+                required: true,
+                name: 'Featured Text',
+            },
+            image: {
+                type: 'string',
+                name: 'Featured Image URL',
+                required: true,
             }
         }
     }
@@ -222,7 +232,7 @@ var schemata = [
         extends: {type: 'string'},
         description: 'Markdown text',
         transformation: function (callback) {
-            callback(null, md.parse(this));
+            callback(null, md.parse(this.toString()));
         }
     }
 ];

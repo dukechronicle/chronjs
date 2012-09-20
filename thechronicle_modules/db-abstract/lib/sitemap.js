27b92fc83cbb8511d5ef21c02a8eac6dd4cba91a
Jim Posen <jim.posen@gmail.com>
2012-09-19 20:37:10 -0400

    Wrote a db.sitemap.saveSitemap function that probably doesn't work.

---
+++ thechronicle_modules/db-abstract/lib/sitemap.js
@@ -0,0 +1,25 @@
+var sitemap = exports;
+
+var db = require('./db');
+
+
+sitemap.saveSitemap = function (type, index, attachment, callback) {
+    sitemap.get(type, index, function (err, docs) {
+        if (err) callback(err);
+        else if (docs.length === 0) {
+            db.save({
+                type: 'sitemap',
+                sitemapType: type,
+                index: index,
+            }, function (err, doc) {
+                db.saveAttachment(doc, attachment, callback);
+            });
+        }
+        else if (docs.length === 1) {
+            db.saveAttachment(docs[0], attachment, callback);
+        }
+        else {
+            return callback('Multiple ' + type + ' sitemaps with index ' + index);
+        }
+    });
+};

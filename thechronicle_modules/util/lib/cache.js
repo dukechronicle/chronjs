11a5e576b8304f092df31db0e64fa5367ff25de8
Jim Posen <jim.posen@gmail.com>
2012-10-23 12:43:58 -0400

    Cache module uses redis now.

--- thechronicle_modules/util/lib/cache.js
+++ thechronicle_modules/util/lib/cache.js
@@ -1,43 +1,45 @@
+var _ = require('underscore');
 var crypto = require('crypto');
 
-var cache = exports;
+var log = require('../../log');
+var redis = require('../../redisclient');
+var util = require('../../util');
 
-var cacheStorage = {};
+var SALT = util.randomString(100);
 
-// ttl in milliseconds
-cache.get = function(ttl, callback, func) {
-	var time = new Date().getTime();
-	var md5sum = crypto.createHash('md5');
-	md5sum.update(callback.toString());
-	var hash = md5sum.digest('hex');
-	// Cache Miss
-	if (typeof cacheStorage[hash] == "undefined" || cacheStorage[hash]['expires'] <= time) {
-		// If cached is less than 5min expired, return anyway, but update cache
-		if (typeof cacheStorage[hash] != "undefined" && cacheStorage[hash]['expires'] - 300000 <= time) {
-			callback(null, cacheStorage[hash].result);
-			func(function(err, result) {
-				if (err == null) {
-					cacheStorage[hash] = {
-						result : result,
-						expires : time + ttl
-					};
-				}
-			});
-		} else {
-			func(function(err, result) {
-				if (err) callback(err)
-				cacheStorage[hash] = {
-					result : result,
-					expires : time + ttl
-				};
-				callback(null, result);
-			});
-		}
-	} else { // Cache Hit
-		callback(null, cacheStorage[hash].result);
-	}
-}
+
+var cache = module.exports = function (expireTime, func) {
+    return function () {
+        var args = Array.prototype.slice.call(arguments);
+        var callback = args.pop();
+        if (typeof(callback) !== 'function')
+            throw new Error('Cache only works for asynchronous functions');
+
+	    var md5sum = crypto.createHash('md5');
+	    md5sum.update(func.toString());
+        md5sum.update(JSON.stringify(args));
+        md5sum.update(SALT);
+	    var redisKey = md5sum.digest('hex');
+
+        redis.client.get(redisKey, function(err, res) {
+            if (!err && res) return callback(null, JSON.parse(res));
+
+            log.notice('Cache miss');
+            args.push(function (err, result) {
+                if (err) return callback(err);
+                redis.client.set(redisKey, JSON.stringify(result), function (err) {
+                    if (err) return log.error(err);
+                    redis.client.expire(redisKey, expireTime, function (err) {
+                        if (err) log.error(err);
+                    });
+                });
+                callback(null, result);
+            });
+            func.apply(this, args);
+        });
+    };
+};
 
 cache.bust = function() {
-	cacheStorage = {};
-}
+    SALT = util.randomString(100);
+};

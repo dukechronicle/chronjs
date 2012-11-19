25a163a71dbe78539375317796ed37ce147eb445
Jim Posen <jim.posen@gmail.com>
2012-11-19 15:40:02 -0500

    Removed openx script.

--- public/scripts/site/main.js
+++ public/scripts/site/main.js
@@ -4,7 +4,7 @@ require.config({
 
 require(['cs!common/main','cs!site/align','cs!site/article','site/category-box',
          'site/scrollLoad','site/slideshow/frontpage-slideshow','site/poll',
-         'site/slideshow/slideshow-right','cs!site/openx',  'lib/jquery.cookie',
+         'site/slideshow/slideshow-right','lib/jquery.cookie',
          'site/facebook', 'cs!site/countdown', 'cs!site/today', 'cs!site/navscroll'],
         function (main) {
             main.apply(this, arguments);
